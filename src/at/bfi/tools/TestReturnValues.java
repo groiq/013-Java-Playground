93b222e739989bc7652ea7c123443af74ad59bca
groiq <35578027+groiq@users.noreply.github.com>
2019-02-25 11:05:44 +0100

    test handling of return values

---
+++ src/at/bfi/tools/TestReturnValues.java
@@ -0,0 +1,43 @@
+package at.bfi.tools;
+
+public class TestReturnValues {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This class is to test some return values.");
+		
+		System.out.println("First, define a method that changes an array.");
+		
+		System.out.println("(i.e. takes an array, modifies it and returns it.)");
+		
+		System.out.println("We will start with an array where arr[i] = i. We will create a new array that is one longer.");
+		
+		System.out.println("Then, call this method in various ways.");
+		
+		System.out.println("First, assign the new value as usual.");
+		
+		System.out.println("arr = myMethod(arr);");
+		
+		System.out.println("then, call the method without assigning anything.");
+		
+		System.out.println("myMethod(arr);");
+		
+		System.out.println("then, call the method from inside a sysout().");
+		
+		System.out.println("pprint(myMethod(arr));");
+
+		System.out.println("Come to think of it, let's do a second method that doesn't create a new array, but changes the array in-place.");
+		
+		System.out.println("let's say it increases each element of the array by one.");
+		
+		System.out.println("and a third method that creates a new array - but assigns it back to the input.");
+		
+		System.out.println("again, call this method once with assignment, once without assignment and once in-place.");
+		
+		
+		
+		
+
+	}
+
+}
