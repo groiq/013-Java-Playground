3b37cefd7e969fd535a79aa3a14de719f08a4428
groiq <35578027+groiq@users.noreply.github.com>
2019-02-27 15:21:37 +0100

    tax calculator assignment: set up output methods

--- src/at/bfi/assignments/conditionals/TaxCalculator.java
+++ src/at/bfi/assignments/conditionals/TaxCalculator.java
@@ -15,18 +15,36 @@ package at.bfi.assignments.conditionals;
 
 public class TaxCalculator {
 	
-	static int[] thresholds = {1500,2500,3500};
-	static int[] rates = {20,35,45,50};
+	private static int[] thresholds = {1500,2500,3500};
+	private static int[] rates = {20,35,45,50};
 	
 	// method that calculates tax and returns an int[] containing gross, tax rate, tax and net
+	public static int[] taxStats(int gross) {
+		int[] stats = new int[4];
+		stats[0] = gross;
+		
+		return stats;
+	}
+
+	
 	
 	// another method that toStrings() that thing
+	public static String statsAsString(int[] stats) {
+		return "gross: " + stats[0] + ", tax rate: " + stats[1] + ", tax: " + stats[2] + ", net: " + stats[3] + ".";
+	}
+	public static String statsAsString(int gross) {
+		return statsAsString(taxStats(gross));
+	}
 	
-	// alternative: just make a void method that prints everything
 
 	public static void main(String[] args) {
 		
 		// test getTax method with representative values
+		for (int i = 1000; i < 5000; i += 1000) {
+//			System.out.println(i);
+			System.out.println(statsAsString(i));
+		}
+		
 
 	}
 
