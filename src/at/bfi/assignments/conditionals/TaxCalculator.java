e7d59c102e6a4d97baa26324a9907174558a1e6d
groiq <35578027+groiq@users.noreply.github.com>
2019-02-27 15:09:07 +0100

    tax calculator assignment: lay out basic structure

--- src/at/bfi/assignments/conditionals/TaxCalculator.java
+++ src/at/bfi/assignments/conditionals/TaxCalculator.java
@@ -1,22 +1,32 @@
 package at.bfi.assignments.conditionals;
 
+/**
+ * assignment:
+ * 
+ * Implement an income tax calculator. You should print the correct tax rate and
+ * the resulting net income. the following progressive tax rates apply (numbers
+ * are gross income): 
+ * 0 - 1500 EUR -> 20% 
+ * 1501 - 2500 EUR -> 35% 
+ * 2501 - 3500 EUR -> 45% 
+ * 3501 and above -> 50%
+ */
+
+
 public class TaxCalculator {
 	
+	static int[] thresholds = {1500,2500,3500};
+	static int[] rates = {20,35,45,50};
 	
-	/**
-	 * assignment:
-	 * 
-	 * Implement an income tax calculator. You should print the correct tax rate and
-	 * the resulting net income. the following progressive tax rates apply (numbers
-	 * are gross income): 
-	 * 0 - 1500 EUR -> 20% 
-	 * 1501 - 2500 EUR -> 35% 
-	 * 2501 - 3500 EUR -> 45% 
-	 * 3501 and above -> 50%
-	 */
+	// method that calculates tax and returns an int[] containing gross, tax rate, tax and net
+	
+	// another method that toStrings() that thing
+	
+	// alternative: just make a void method that prints everything
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// test getTax method with representative values
 
 	}
 
