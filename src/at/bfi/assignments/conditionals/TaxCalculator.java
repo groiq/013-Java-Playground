4ff76eba27bfd3b47691e0e4c04b50250b41a459
groiq <35578027+groiq@users.noreply.github.com>
2019-02-27 13:28:27 +0100

    tax calc assignment: added assignment text

--- src/at/bfi/assignments/conditionals/TaxCalculator.java
+++ src/at/bfi/assignments/conditionals/TaxCalculator.java
@@ -1,6 +1,19 @@
 package at.bfi.assignments.conditionals;
 
 public class TaxCalculator {
+	
+	
+	/**
+	 * assignment:
+	 * 
+	 * Implement an income tax calculator. You should print the correct tax rate and
+	 * the resulting net income. the following progressive tax rates apply (numbers
+	 * are gross income): 
+	 * 0 - 1500 EUR -> 20% 
+	 * 1501 - 2500 EUR -> 35% 
+	 * 2501 - 3500 EUR -> 45% 
+	 * 3501 and above -> 50%
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
