37d0c20a4161c5f3c69ae43dd0fd5a2e60287589
groiq <35578027+groiq@users.noreply.github.com>
2019-03-03 16:45:30 +0100

    tinkered a bit, but that must work better

--- src/at/bfi/assignments/conditionals/taxCalculator/v4/TaxCalculator.java
+++ src/at/bfi/assignments/conditionals/taxCalculator/v4/TaxCalculator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TaxCalculator {
 	
 	private static int[] rates = {20,35,45,50};
-	private static int[] thresholds = {0,1500,2500,3500};
+	private static int[] thresholds = {1500,2500,3500};
 	
 	public static int[] asArray(int i) {
 //		System.out.println("one-liner: asArray(" + i + ")");
@@ -40,21 +40,25 @@ public class TaxCalculator {
 		int totalTax = 0;
 		int totalNet = 0;
 		
-		int remainingGross = gross; // could even use gross itself, but I'll use a better name
+		int remainingGross = gross;
+		int pos = 0;
+		int currChunk = thresholds[0];
 		
-		int curGrossChunk;
-		int i;
-		for (i = 0; i < thresholds.length; i++) {
-			System.out.println("remaining gross: " + remainingGross + ", cur threshold: " + thresholds[i] + 
-					", next threshold: " + thresholds[i+1]);
-			if (i < thresholds.length - 1) {
-				curGrossChunk = thresholds[i+1] - thresholds[i];
-				
+		while (remainingGross >= currChunk) {
+			
+			remainingGross -= currChunk;
+			
+			
+			int currTax = ( currChunk * rates[pos] ) / 100;
+			totalTax += currTax;
+			
+			pos += 1;
+			if (pos >= thresholds.length) {
+				break;
 			}
-					
+			currChunk = thresholds[pos] - thresholds[pos - 1];
+			
 		}
-		
-		
 		
 		
 		totalNet = gross - totalTax;
