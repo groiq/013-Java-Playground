e148e6301c4dfb85187a7bbfe47121f598faedaf
groiq <35578027+groiq@users.noreply.github.com>
2019-03-04 20:38:12 +0100

    added fibonacci by threshold

---
+++ src/at/bfi/assignments/loops/fibonacci/ByThreshold.java
@@ -0,0 +1,25 @@
+package at.bfi.assignments.loops.fibonacci;
+
+public class ByThreshold {
+
+	public static void main(String[] args) {
+		
+		int pprev = 0;
+		int prev = 1;
+		
+		System.out.print(pprev + " " + prev + " ");
+		
+		int curr = pprev + prev;
+		while (curr <= 100) {
+			System.out.print(curr + " ");
+			pprev = prev;
+			prev = curr;
+			curr = pprev + prev;
+			
+		}
+		
+		System.out.println();
+
+	}
+
+}
