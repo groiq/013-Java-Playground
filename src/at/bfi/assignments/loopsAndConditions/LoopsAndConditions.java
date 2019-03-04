c17da8ef3fc66c539993a68bca22bb16d3e39c3e
groiq <35578027+groiq@users.noreply.github.com>
2019-03-04 11:21:21 +0100

    set up assignments on loops and conditions

---
+++ src/at/bfi/assignments/loopsAndConditions/LoopsAndConditions.java
@@ -0,0 +1,22 @@
+/**
+ * assignments:
+ * 
+ * 1. Print the numbers from 1 to 20.
+ * If a number n is divisible by three, print (n-1)+(n-2) instead.
+ * 
+ * 2. Print all Fibonacci numbers *smaller-equal* than 100 (by threshold!).
+ * 
+ * 3. Print all primes smaller-equal than 100.
+ */
+
+package at.bfi.assignments.loopsAndConditions;
+
+public class LoopsAndConditions {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
