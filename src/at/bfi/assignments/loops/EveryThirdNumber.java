84c0b58e841fa9ea6e59f7d30bf42faff766051e
groiq <35578027+groiq@users.noreply.github.com>
2019-03-04 20:30:26 +0100

    finalized every third number exercise

--- src/at/bfi/assignments/loops/EveryThirdNumber.java
+++ src/at/bfi/assignments/loops/EveryThirdNumber.java
@@ -4,21 +4,43 @@ import java.util.Arrays;
 
 public class EveryThirdNumber {
 	
-	private static int[] everyThird(int threshold) {
-		
-		return null;
+	private static void everyThird(int threshold) {
+		int pprev = 1;
+		int prev = 2;
+//		int cur = 3;
+		System.out.print(pprev + " " + prev + " ");
+		for (int i = 3; i <= threshold; i++) {
+			if (i % 3 != 0) {
+				System.out.print(i + " "); 
+			} else {
+				System.out.print(pprev + prev + " ");
+			}
+			pprev = prev;
+			prev = i;
+		}
+		System.out.println();
 	}
 	
-	private static int[] everyThirdShortcut(int threshold) {
+	private static void everyThirdShortcut(int threshold) {
+		for (int i = 1; i <= threshold; i++) {
+			if (i % 3 != 0) {
+				System.out.print(i + " ");
+			} else {
+				// (i-2)+(i-1) = i + i - 2 - 1 = 2i - 3
+				System.out.print((2*i-3)+" ");
+			}
+		}
+		System.out.println();
 		
-		return null;
 	}
 
 	public static void main(String[] args) {
 		
 		int threshold = 20;
-		System.out.println(Arrays.toString(everyThird(threshold)));
-		System.out.println(Arrays.toString(everyThirdShortcut(threshold)));
+//		System.out.println(Arrays.toString(everyThird(threshold)));
+//		System.out.println(Arrays.toString(everyThirdShortcut(threshold)));
+		everyThird(threshold);
+		everyThirdShortcut(threshold);
 
 	}
 
