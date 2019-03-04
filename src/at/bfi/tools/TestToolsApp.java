7483227abed59e83427c36f370e183b0a4f899cc
groiq <35578027+groiq@users.noreply.github.com>
2019-03-04 12:40:08 +0100

    set up tools test class

---
+++ src/at/bfi/tools/TestToolsApp.java
@@ -0,0 +1,14 @@
+/**
+ * Testing ground for any tools I might write in this package.
+ */
+
+package at.bfi.tools;
+
+public class TestToolsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
