7369355f003ac2f597146b4b39a421dfd182453f
groiq <35578027+groiq@users.noreply.github.com>
2019-03-04 20:18:11 +0100

    set up every third exercise

---
+++ src/at/bfi/assignments/loops/EveryThirdNumber.java
@@ -0,0 +1,25 @@
+package at.bfi.assignments.loops;
+
+import java.util.Arrays;
+
+public class EveryThirdNumber {
+	
+	private static int[] everyThird(int threshold) {
+		
+		return null;
+	}
+	
+	private static int[] everyThirdShortcut(int threshold) {
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		
+		int threshold = 20;
+		System.out.println(Arrays.toString(everyThird(threshold)));
+		System.out.println(Arrays.toString(everyThirdShortcut(threshold)));
+
+	}
+
+}
