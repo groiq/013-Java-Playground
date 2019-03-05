3cb6e5f0a8ef78baf9fb90439005d47777cdaca1
groiq <35578027+groiq@users.noreply.github.com>
2019-03-05 09:32:06 +0100

    added simple prime numbers program

---
+++ src/at/bfi/assignments/loops/PrimeNumbersWithoutArray.java
@@ -0,0 +1,23 @@
+package at.bfi.assignments.loops;
+
+public class PrimeNumbersWithoutArray {
+
+	public static void main(String[] args) {
+		
+		int threshold = 100;
+		
+		System.out.print("|");
+		for (int i = 2; i <= threshold; i++) {
+			boolean prime = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					prime = false;
+				}
+			}
+			if (prime) System.out.print(i + "|");
+		}
+		System.out.println();
+
+	}
+
+}
