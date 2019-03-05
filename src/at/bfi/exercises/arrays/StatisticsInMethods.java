9f30f6f42bcca6e865382b9e22f96f4bb9683c58
groiq <35578027+groiq@users.noreply.github.com>
2019-03-05 20:22:14 +0100

    Create StatisticsInMethods.java

---
+++ src/at/bfi/exercises/arrays/StatisticsInMethods.java
@@ -0,0 +1,29 @@
+package at.bfi.exercises.arrays;
+
+public class StatisticsInMethods {
+
+	public static void main(String[] args) {
+		
+		double[] minMax;
+		minMax = new double[] {3.0,4.0,1.0,2.0};
+		
+		double[] avg;
+		avg = new double[] {2.0,4.0};
+		
+		double[] noVarianceData = {2.0,2.0,2.0,2.0};
+		double[] lowVarianceData = {2.0,2.0,6.0,6.0};
+		double[] higherVarianceData = {4.0,4.0,12.0,12.0};
+
+	}
+	
+	// minimum
+	
+	// maximum
+	
+	// sum
+	
+	// average
+	
+	// standard deviation
+
+}
