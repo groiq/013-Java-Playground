db7a17c655a8cb1d5ece10b405fa1547d59e9538
groiq <35578027+groiq@users.noreply.github.com>
2019-03-05 11:58:21 +0100

    improved prime numbers program: set up assignment

---
+++ src/at/bfi/assignments/loops/PrimeNumbers03.java
@@ -0,0 +1,27 @@
+/**
+ * assignment:
+ * print all prime numbers smaller or equal to 100.
+ */
+
+package at.bfi.assignments.loops;
+
+import java.util.ArrayList;
+
+public class PrimeNumbers03 {
+
+	public static void main(String[] args) {
+		
+		int maxNum = 100;
+		ArrayList<Integer> primes = getPrimes(maxNum);
+		System.out.println(primes);
+
+	}
+
+	private static ArrayList<Integer> getPrimes(int maxNum) {
+		
+		
+		
+		return null;
+	}
+
+}
