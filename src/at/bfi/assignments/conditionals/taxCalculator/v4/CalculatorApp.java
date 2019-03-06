7de84f43afbd6ad9ebee695ba62ad9829f312dd3
groiq <35578027+groiq@users.noreply.github.com>
2019-03-06 13:46:42 +0100

    add tax rate to output and document everything

--- src/at/bfi/assignments/conditionals/taxCalculator/v4/CalculatorApp.java
+++ src/at/bfi/assignments/conditionals/taxCalculator/v4/CalculatorApp.java
@@ -1,23 +1,7 @@
 /**
- * assignment:
+ * Implements an income tax calculator.
  * 
- * Implement an income tax calculator. You should print the correct tax rate and
- * the resulting net income. the following progressive tax rates apply (numbers
- * are gross income): 
- * 0 - 1500 EUR -> 20% 
- * 1501 - 2500 EUR -> 35% 
- * 2501 - 3500 EUR -> 45% 
- * 3501 and above -> 50%
- * 
- * Note that the assignment doesn't specify a tax progression method.
- * The correct tax calculation is as such:
- * If you earn 400 Eur, then
- * 1500 Eur are taxed with 20%,
- * 1000 Eur (1501-2500) with 35%,
- * 1000 Eur (2501-3500) with 45%,
- * and 500 Eur (3501-4000) with 50%.
- * 
- * 
+ * This class provides a main method to test TaxCalculator.
  */
 
 
@@ -34,13 +18,11 @@ public class CalculatorApp {
 			int[] stats = TaxCalculator.asArray(i);
 			System.out.println("Calculate and return the array: ");
 			System.out.println(Arrays.toString(stats));
-			System.out.println("fetch a string from the array: " );
+			System.out.println("generate a string from the array: " );
 			System.out.println(TaxCalculator.asString(stats));
 			System.out.println("calculate from the original gross value and return as a string: ");
 			System.out.println(TaxCalculator.asString(i));
 			System.out.println();
-			
-			
 		}
 
 	}
