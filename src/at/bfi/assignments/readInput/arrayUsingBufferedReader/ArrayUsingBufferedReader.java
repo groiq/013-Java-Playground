81a1fb238509ff4063836b9933167b852c56c90b
groiq <35578027+groiq@users.noreply.github.com>
2019-03-07 14:31:12 +0100

    set up exercise on buffered reader

---
+++ src/at/bfi/assignments/readInput/arrayUsingBufferedReader/ArrayUsingBufferedReader.java
@@ -0,0 +1,10 @@
+package at.bfi.assignments.readInput.arrayUsingBufferedReader;
+
+public class ArrayUsingBufferedReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
