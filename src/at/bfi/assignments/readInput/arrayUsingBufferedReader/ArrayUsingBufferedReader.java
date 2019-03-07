31b266e81d2c8f070b41db92359aa858240efe5d
groiq <35578027+groiq@users.noreply.github.com>
2019-03-07 15:30:00 +0100

    add assignment specs

--- src/at/bfi/assignments/readInput/arrayUsingBufferedReader/ArrayUsingBufferedReader.java
+++ src/at/bfi/assignments/readInput/arrayUsingBufferedReader/ArrayUsingBufferedReader.java
@@ -2,8 +2,15 @@ package at.bfi.assignments.readInput.arrayUsingBufferedReader;
 
 public class ArrayUsingBufferedReader {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * create an array of size 7.
+		 * The value of each array entry is asked through the command line.
+		 * In the end, print the array.
+		 */
+		
 
 	}
 
