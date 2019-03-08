bbb40835043ed01d494863b40cf58227964a8e6d
groiq <35578027+groiq@users.noreply.github.com>
2019-03-08 10:29:53 +0100

    countOccurrences: code works

--- src/at/bfi/assignments/stringMethods/countOccurrences.java
+++ src/at/bfi/assignments/stringMethods/countOccurrences.java
@@ -1,37 +1,46 @@
 /**
- * Counts occurrences of a search pattern inside a string.
+ * Provides methods that invoke <String>.indexOf() multiple times.
  */
 
 package at.bfi.assignments.stringMethods;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.Arrays;
 
 public class countOccurrences {
 	
-	public static int countOccurrences(String text, String searchPattern) {
+	/**
+	 * Counts the occurrences of a substing inside a string.
+	 * @param text - the string to be queried
+	 * @param substring - the substring to be searched for
+	 * @return the number of occurrences of the substring inside the text
+	 */
+	public static int countOccurrences(String text, String substring) {
 		int result = 0;
-		
-		int pos = text.indexOf(searchPattern);
-		while (pos > -1) {
+		int pos = text.indexOf(substring);
+		while (pos > -1 && pos < text.length()) {
 			result += 1;
-			pos = text.indexOf(searchPattern, pos+1);
+			pos = text.indexOf(substring, pos+1);
 		}
-		
-
-		
 		return result;
 	}
 	
-	public static int[] allIndicesOf(String text, String searchPattern) {
-		int[] interim = new int[text.length()/searchPattern.length()];
+	/**
+	 * Looks up all occurrences of a search pattern inside a string.
+	 * 
+	 * @param text 
+	 * @param substring
+	 * @return
+	 */
+	public static int[] allIndicesOf(String text, String substring) {
+		int[] interim = new int[text.length()/substring.length()];
 		int counter = 0;
 		
-		int pos = text.indexOf(searchPattern);
+		int pos = text.indexOf(substring);
 		while (pos > -1) {
 			interim[counter] = pos;
 			counter++;
-			pos = text.indexOf(searchPattern, pos+1);
+			pos = text.indexOf(substring, pos+1);
 		}
 		
 		int[] result = new int[counter];
@@ -42,20 +51,21 @@ public class countOccurrences {
 		return result;
 	}
 	
-//	public static Integer[] allIndicesOf(String text, String searchPattern) {
-//		ArrayList<int> result = new ArrayList<int>();
-//		
-//		return result.toArray();
-//	}
 
 	public static void main(String[] args) {
 		
 		String text;
 		String searchPattern;
 		text = "wenn fliegen hinter fliegen fliegen, fliegen fliegen fliegen nach";
+		text = "fliegen und fliegen";
+		text = "fliegen";
 		searchPattern = "fliegen";
 //		text = "blablabla ";
-//		searchPattern = "bla";
+		searchPattern = "bla";
+//		text = "sosox";
+//		searchPattern = "so";
+//		text = "xxxo";
+//		searchPattern = "x";
 		
 		System.out.println(countOccurrences(text, searchPattern));
 		System.out.println(Arrays.toString(allIndicesOf(text, searchPattern)));
