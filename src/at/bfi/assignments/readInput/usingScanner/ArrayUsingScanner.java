cedfe577f15656a41776c88308804117f250a8f0
groiq <35578027+groiq@users.noreply.github.com>
2019-03-08 10:36:23 +0100

    set up Scanner exercise

---
+++ src/at/bfi/assignments/readInput/usingScanner/ArrayUsingScanner.java
@@ -0,0 +1,26 @@
+package at.bfi.assignments.readInput.usingScanner;
+
+import java.util.Scanner;
+
+public class ArrayUsingScanner {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * create an array of size 7.
+		 * The value of each array entry is asked through the command line.
+		 * In the end, print the array.
+		 */
+		
+		int size = 7;
+		int[] theArray = new int[size];
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter " + theArray.length + " numbers:");
+		for (int i = 0; i < theArray.length; i++) {
+			
+		}
+
+	}
+
+}
