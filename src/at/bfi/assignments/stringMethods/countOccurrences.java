49f59c066624362614fe119b02d065b9aedbe2ad
groiq <35578027+groiq@users.noreply.github.com>
2019-03-08 09:55:03 +0100

    count occurrences exercise: return an array of all occurrences

--- src/at/bfi/assignments/stringMethods/countOccurrences.java
+++ src/at/bfi/assignments/stringMethods/countOccurrences.java
@@ -5,6 +5,7 @@
 package at.bfi.assignments.stringMethods;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class countOccurrences {
 	
@@ -22,6 +23,25 @@ public class countOccurrences {
 		return result;
 	}
 	
+	public static int[] allIndicesOf(String text, String searchPattern) {
+		int[] interim = new int[text.length()/searchPattern.length()];
+		int counter = 0;
+		
+		int pos = text.indexOf(searchPattern);
+		while (pos > -1) {
+			interim[counter] = pos;
+			counter++;
+			pos = text.indexOf(searchPattern, pos+1);
+		}
+		
+		int[] result = new int[counter];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = interim[i];
+		}
+		
+		return result;
+	}
+	
 //	public static Integer[] allIndicesOf(String text, String searchPattern) {
 //		ArrayList<int> result = new ArrayList<int>();
 //		
@@ -34,8 +54,11 @@ public class countOccurrences {
 		String searchPattern;
 		text = "wenn fliegen hinter fliegen fliegen, fliegen fliegen fliegen nach";
 		searchPattern = "fliegen";
+//		text = "blablabla ";
+//		searchPattern = "bla";
 		
 		System.out.println(countOccurrences(text, searchPattern));
+		System.out.println(Arrays.toString(allIndicesOf(text, searchPattern)));
 
 	}
 
