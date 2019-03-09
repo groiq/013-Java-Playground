85b7e826ddd4cb808b3f41af225907349231e743
groiq <35578027+groiq@users.noreply.github.com>
2019-03-09 09:28:45 +0100

    IndexOfDIY: documented

--- src/at/bfi/assignments/strings/indexOfDIY/IndexOfDIY.java
+++ src/at/bfi/assignments/strings/indexOfDIY/IndexOfDIY.java
@@ -16,6 +16,12 @@ public class IndexOfDIY {
 
 	}
 
+	/**
+	 * See documentation for indexOf.
+	 * @param text - the string to be queried
+	 * @param wanted - the string to be searched for
+	 * @return the index of the first occurrence
+	 */
 	private static int indexOfDIY(String text, String wanted) {
 
 		int pos = -1;
@@ -23,8 +29,14 @@ public class IndexOfDIY {
 		boolean searching = false;
 		
 		for (int i = 0; i < text.length(); i++) {
+			/*
+			 * If a char in text matches the beginning of wanted, enter search mode
+			 */
 			if (text.charAt(i) == digHere) {
 				searching = true;
+				/*
+				 * In search mode, loop through wanted and compare all chars with the following chars in text
+				 */
 				for (int j = 0; j < wanted.length(); j++) {
 //					System.out.println("i: " + i + ", j: " + j + ", in text: " + text.charAt(i+j) + ", " + "in wanted: " + wanted.charAt(j));
 					if (wanted.charAt(j) != text.charAt(i+j)) {
@@ -32,14 +44,15 @@ public class IndexOfDIY {
 						break;
 					}
 				}
+				/*
+				 * If we've reached the end of wanted without breaking out of search mode, we've found our substring.
+				 */
 				if (searching) {
 					pos = i;
 					break;
 				}
 			}
 		}
-		
-		
 		
 		return pos;
 		
