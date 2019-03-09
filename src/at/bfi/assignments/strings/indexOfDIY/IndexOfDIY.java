60838ca4e18ad65a9cdd2ef6190efc55360c57bd
groiq <35578027+groiq@users.noreply.github.com>
2019-03-09 09:24:53 +0100

    indexOfDIY done, still undocumented

--- src/at/bfi/assignments/strings/indexOfDIY/IndexOfDIY.java
+++ src/at/bfi/assignments/strings/indexOfDIY/IndexOfDIY.java
@@ -1,10 +1,12 @@
 package at.bfi.assignments.strings.indexOfDIY;
 
+import java.util.Iterator;
+
 public class IndexOfDIY {
 
 	public static void main(String[] args) {
 		
-		String text = "There's a treasure hidden somewhere in here.";
+		String text = "There's a trea-treasure hidden somewhere in here.";
 		String wanted = "treasure";
 		String quote = "\"";
 
@@ -17,6 +19,25 @@ public class IndexOfDIY {
 	private static int indexOfDIY(String text, String wanted) {
 
 		int pos = -1;
+		char digHere = wanted.charAt(0);
+		boolean searching = false;
+		
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == digHere) {
+				searching = true;
+				for (int j = 0; j < wanted.length(); j++) {
+//					System.out.println("i: " + i + ", j: " + j + ", in text: " + text.charAt(i+j) + ", " + "in wanted: " + wanted.charAt(j));
+					if (wanted.charAt(j) != text.charAt(i+j)) {
+						searching = false;
+						break;
+					}
+				}
+				if (searching) {
+					pos = i;
+					break;
+				}
+			}
+		}
 		
 		
 		
