ef2bdb5eec10a3c47afcd578d0681cc0ace32633
groiq <35578027+groiq@users.noreply.github.com>
2019-03-09 09:15:16 +0100

    set up exercise: indexOf DIY

---
+++ src/at/bfi/assignments/strings/indexOfDIY/IndexOfDIY.java
@@ -0,0 +1,27 @@
+package at.bfi.assignments.strings.indexOfDIY;
+
+public class IndexOfDIY {
+
+	public static void main(String[] args) {
+		
+		String text = "There's a treasure hidden somewhere in here.";
+		String wanted = "treasure";
+		String quote = "\"";
+
+		int position = indexOfDIY(text, wanted);
+		
+		System.out.println(quote + wanted + quote + " found in " + quote + text + quote + " at position " + position);
+
+	}
+
+	private static int indexOfDIY(String text, String wanted) {
+
+		int pos = -1;
+		
+		
+		
+		return pos;
+		
+	}
+
+}
