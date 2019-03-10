d656feb5e36d2466b9ad8a37918fdefcbc57ded8
groiq <35578027+groiq@users.noreply.github.com>
2019-03-10 16:52:16 +0100

    set up tester class

--- src/at/bfi/tools/passByReferenceAndReturnValues/PassByReferenceAndReturnValues.java
+++ src/at/bfi/tools/passByReferenceAndReturnValues/PassByReferenceAndReturnValues.java
@@ -4,30 +4,59 @@
 
 package at.bfi.tools.passByReferenceAndReturnValues;
 
+import java.util.Arrays;
+
 public class PassByReferenceAndReturnValues {
+	
+	private static int changes = 0;
+//	private static int 
 
 	public static void main(String[] args) {
 		
-		
+		Tester t1 = new Tester("t1",0);
+		Tester t2 = new Tester("t2",0);
+		System.out.println(t1);
+		System.out.println(t2);
 
 	}
 	
-	private static int[] reassignVariable(int[] input) {
-		int counter = input[0];
-		input = new int[input.length];
-		for (int i = 0; i < input.length; i++) {
-			input[i] = i + counter;
-		}
-		return input;
-	}
 	
-	private static int[] changeVariableInPlace(int[] input) {
-		for (int i = 0; i < input.length; i++) {
-			input[i] += 1;
-		}
-		return input;
-	}
-	
-	
+}
 
+ class Tester {
+	
+	 final String name;
+	 int changes;
+	 final int objCount;
+	 private static int runningObjectCounter = 0;
+	 
+	int getChanges() {
+		return changes;
+	}
+	void setChanges(int changes) {
+		this.changes = changes;
+	}
+	String getName() {
+		return name;
+	}
+	int getObjCount() {
+		return objCount;
+	}
+	public Tester(String name, int changes) {
+		super();
+		this.name = name;
+		this.changes = changes;
+		this.objCount = runningObjectCounter;
+		runningObjectCounter += 1;
+	}
+	@Override
+	public String toString() {
+		return "Tester [name=" + name + ", changes=" + changes + ", objCount=" + objCount + "]";
+	}
+	 
+	 
+
+	 
+	
+	 
 }
