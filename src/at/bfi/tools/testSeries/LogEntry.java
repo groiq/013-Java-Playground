e232fa85dd6c7128671bf6f1831cab35f2b9b2e9
groiq <35578027+groiq@users.noreply.github.com>
2019-03-11 11:54:48 +0100

    done a bit on log entries

--- src/at/bfi/tools/testSeries/LogEntry.java
+++ src/at/bfi/tools/testSeries/LogEntry.java
@@ -1,5 +1,19 @@
 package at.bfi.tools.testSeries;
 
 public class LogEntry {
+	
+//	private final TestSeries series;
+	private final TestObject object;
+	private final int index;
+	private final String command;
+	
+	public LogEntry(TestObject object, int index, String command) {
+		super();
+		this.object = object;
+		this.index = index;
+		this.command = command;
+	}
+	
+	
 
 }
