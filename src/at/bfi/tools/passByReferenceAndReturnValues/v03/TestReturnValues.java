011375febcc808fe8b9a615e30ec221aae8be6aa
groiq <35578027+groiq@users.noreply.github.com>
2019-03-12 09:07:00 +0100

    added parameter 'command', added method calls

--- src/at/bfi/tools/passByReferenceAndReturnValues/v03/TestReturnValues.java
+++ src/at/bfi/tools/passByReferenceAndReturnValues/v03/TestReturnValues.java
@@ -8,54 +8,67 @@ public class TestReturnValues {
 		
 		TestObject.startNewTestSeries();
 		
-		TestObject tester = new TestObject();
+		TestObject tester = new TestObject("tester = new TestObject()");
 		
+		tester = sameObjectSameVariable(tester, "tester = sameObjectSameVariable(tester)");
+		sameObjectSameVariable(tester, "sameObjectSameVariable(tester)");
+		tester = sameObjectNewVariable(tester, "tester = sameObjectNewVariable(tester)");
+		sameObjectNewVariable(tester, "sameObjectNewVariable(tester)");
+		tester = newObjectSameVariable(tester, "tester = newObjectSameVariable(tester)");
+		newObjectSameVariable(tester, "newObjectSameVariable(tester)");
+		tester = newObjectNewVariable(tester, "tester = newObjectNewVariable(tester)");
+		newObjectNewVariable(tester, "newObjectNewVariable(tester)");
+		
+		sameObjectSameVariableVoid(tester, "sameObjectSameVariableVoid(tester)");
+		sameObjectNewVariableVoid(tester, "sameObjectNewVariableVoid(tester)");
+		newObjectSameVariableVoid(tester, "newObjectSameVariableVoid(tester)");
+		newObjectNewVariableVoid(tester, "newObjectNewVariableVoid(tester)");
 		
 		
 		TestObject.printOverallLog();
 
 	}
 	
-	private static TestObject sameObjectSameVariable(TestObject input) {
-		input.change();
+	private static TestObject sameObjectSameVariable(TestObject input, String command) {
+		input.change(command);
 		return input;
 	}
 	
-	private static TestObject sameObjectNewVariable(TestObject input) {
+	private static TestObject sameObjectNewVariable(TestObject input, String command) {
 		TestObject output = input;
-		output.change();
+		output.change(command);
 		return output;
 	}
 	
-	private static TestObject newObjectSameVariable(TestObject input) {
-		input = new TestObject();
-		input.change();
+	private static TestObject newObjectSameVariable(TestObject input, String command) {
+		input = new TestObject(command);
+		input.change(command);
 		return input;
 	}
 	
-	private static TestObject newObjectNewVariable(TestObject input) {
-		TestObject output = new TestObject();
-		output.change();
+	private static TestObject newObjectNewVariable(TestObject input, String command) {
+		TestObject output = new TestObject(command);
+		output.change(command);
 		return output;
 	}
 	
-	private static void sameObjectSameVariableVoid(TestObject input) {
-		input.change();
+	private static void sameObjectSameVariableVoid(TestObject input, String command) {
+		input.change(command);
 	}
 	
-	private static void sameObjectNewVariableVoid(TestObject input) {
+	private static void sameObjectNewVariableVoid(TestObject input, String command) {
 		TestObject output = input;
-		output.change();
+		output.change(command);
 	}
 	
-	private static void newObjectSameVariableVoid(TestObject input) {
-		input = new TestObject();
-		input.change();
+	private static void newObjectSameVariableVoid(TestObject input, String command) {
+		input = new TestObject(command);
+		input.change(command);
 	}
 	
-	private static void newObjectNewVariableVoid(TestObject input) {
-		TestObject output = new TestObject();
-		output.change();
+	private static void newObjectNewVariableVoid(TestObject input, String command) {
+		TestObject output = new TestObject(command);
+		output.change(command);
 	}
 	
 
