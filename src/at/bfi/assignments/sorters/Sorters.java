63f5da9f26b89547acb531524f206535bd0c5deb
groiq <35578027+groiq@users.noreply.github.com>
2019-03-14 15:22:14 +0100

    m: typo

--- src/at/bfi/assignments/sorters/Sorters.java
+++ src/at/bfi/assignments/sorters/Sorters.java
@@ -12,7 +12,7 @@ public class Sorters {
 	/**
 	 * An implementation of the sorting algorithm described at 
 	 * http://www.dangermouse.net/esoteric/intelligentdesignsort.html
-	 * Since this is very easy to implement, it' useful for testing method calls.
+	 * Since this is very easy to implement, it's useful for testing method calls.
 	 * @param input the array to be sorted
 	 * @return the sorted array
 	 */
