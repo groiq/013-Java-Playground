7a1ca42bf08e744fb0f1c4163e971e7e7c7e4c85
groiq <35578027+groiq@users.noreply.github.com>
2019-03-18 08:30:13 +0100

    append and delete: done code, need to verify it

--- src/com/hackerrank/exercises/algorithms/ex03AppendAndDelete/Ex03AppendAndDelete.java
+++ src/com/hackerrank/exercises/algorithms/ex03AppendAndDelete/Ex03AppendAndDelete.java
@@ -10,7 +10,42 @@ public class Ex03AppendAndDelete {
     
     static boolean isAppendable(String firstWord, String secondWord, int steps) {
     	
-    	return false;
+    	boolean result = false;
+    	
+    	int equalUntil = 0;
+    	
+    	while (equalUntil < firstWord.length() && equalUntil < secondWord.length() 
+    			&& firstWord.charAt(equalUntil) == secondWord.charAt(equalUntil)) {
+//    		System.out.print(firstWord.charAt(equalUntil));
+    		equalUntil++;
+    	}
+//    	System.out.println();
+    	
+    	int firstSubstrLen = firstWord.length() - equalUntil;
+    	int secondSubstrLen = secondWord.length() - equalUntil;
+    	
+//    	for (int i = 0; i < equalUntil; i++) {
+//			System.out.print(firstWord.charAt(i));
+//		}
+//    	System.out.print(": " + equalUntil + ", ");
+//    	for (int i = equalUntil; i < firstWord.length(); i++) {
+//			System.out.print(firstWord.charAt(i));
+//		}
+//    	System.out.print(": " + firstSubstrLen + ", ");
+//    	for (int i = equalUntil; i < secondWord.length(); i++) {
+//			System.out.print(secondWord.charAt(i));
+//		}
+//    	System.out.println(": " + secondSubstrLen);
+//    	
+    	int stepsForEqual = steps - (firstSubstrLen + secondSubstrLen);
+    	
+    	if (stepsForEqual >= 0) {
+    		if (stepsForEqual >= equalUntil * 2 || stepsForEqual % 2 == 0) {
+    			result = true;
+    		}
+    	}
+    	
+    	return result;
     }
 
     static void printResult(String firstWord,String secondWord,int steps) {
