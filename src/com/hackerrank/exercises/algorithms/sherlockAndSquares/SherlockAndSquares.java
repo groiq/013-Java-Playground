b52d200a511808b4ac58934511b1462b068c3e13
groiq <35578027+groiq@users.noreply.github.com>
2019-03-18 13:44:18 +0100

    set up tests

--- src/com/hackerrank/exercises/algorithms/sherlockAndSquares/SherlockAndSquares.java
+++ src/com/hackerrank/exercises/algorithms/sherlockAndSquares/SherlockAndSquares.java
@@ -6,11 +6,27 @@ public class SherlockAndSquares {
 
 	public static void main(String[] args) {
 		
-		int threshold;
-		threshold = 11;
-		System.out.println(Arrays.toString(CalculateSquares.bySequence(threshold)));
-		System.out.println(Arrays.toString(CalculateSquares.bySquaring(threshold)));
+//		int threshold;
+//		threshold = 11;
+//		System.out.println(Arrays.toString(CalculateSquares.bySequence(threshold)));
+//		System.out.println(Arrays.toString(CalculateSquares.bySquaring(threshold)));
+//		
+		System.out.println("3,2,0");
+		System.out.println(squares(24,49));
+		System.out.println(squares(3,9));
+		System.out.println(squares(17,24));
+		
 
 	}
+	
+    // Complete the squares function below.
+    static int squares(int a, int b) {
+    	int count = 0;
+
+    	
+    	
+    	return count;
+
+    }
 
 }
