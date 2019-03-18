c680a294b1937a6db25dcfd735e486a269da8dbc
groiq <35578027+groiq@users.noreply.github.com>
2019-03-18 17:41:06 +0100

    new attempt, still doesn't work

--- src/com/hackerrank/exercises/algorithms/sherlockAndSquares/SherlockAndSquares.java
+++ src/com/hackerrank/exercises/algorithms/sherlockAndSquares/SherlockAndSquares.java
@@ -11,14 +11,25 @@ public class SherlockAndSquares {
 //		System.out.println(Arrays.toString(CalculateSquares.bySequence(threshold)));
 //		System.out.println(Arrays.toString(CalculateSquares.bySquaring(threshold)));
 //		
-		System.out.println("3,2,0");
-		System.out.println(squares(24,49));
-		System.out.println(squares(3,9));
-		System.out.println(squares(17,24));
-		System.out.println(squares(0,4));
-		System.out.println(squares(1,4));
-		System.out.println(squares(35,70));
+//		for (int i = 0; i < 33; i++) {
+//			System.out.println(i + " -> " + sqrtRange(i)[0] + "-" + sqrtRange(i)[1]);
+////			System.out.println(sqr(i));
+//		}
+
+		
+//		System.out.println("3,2,0");
+//		System.out.println(squares(24,49));
+//		System.out.println(squares(3,9));
+//		System.out.println(squares(17,24));
+//		System.out.println(squares(0,4));
+//		System.out.println(squares(1,4));
+//		System.out.println(squares(35,70));
+//		System.out.println(squares(4,9));
+//		
 		System.out.println(squares(4,9));
+		System.out.println(squares(5,9));
+		System.out.println(squares(4,8));
+		System.out.println(squares(5, 8));
 		
 
 	}
@@ -27,23 +38,56 @@ public class SherlockAndSquares {
     static int squares(int a, int b) {
     	int count = 0;
 
-    	int i = 0;
-    	int j = 0;
+    	int upper = sqrtRange(b)[0];
+    	int lower = sqrtRange(a)[1];
     	
-    	while(j < a) {
-    		i++;
-    		j = i * i;
-    	}
-    	while(j <= b) {
-//    		j = i*i;
-    		count++;
-    		System.out.println("i:" + i + ",j:" + j + ",count:" + count);
-    		i++;
-    		j = i*i;
-    	}
+    	System.out.println(lower + "-" + upper);
+
+    	count = (upper - lower) + 1;
     	
     	return count;
 
     }
+    
+//    static int sqr(int input) {
+//    	return input * input;
+//    }
+    
+    static int[] sqrtRange(int input) {
+    	
+    	if (input == 1) {
+    		return new int[]{1,1};
+    	}
+    	
+    	int[] range = new int[2];
+    	
+    	int min = 0;
+    	int max = input;
+    	int currGuess = input/2;
+    	int currSqr = currGuess * currGuess;
+    	
+    	while ( currSqr != input && (max-min)>1 ) {
+    		if (currSqr < input) {
+    			min = currGuess;
+    		} else {
+    			max = currGuess;
+    		}
+    		currGuess = (max+min)/2;
+    		currSqr = currGuess * currGuess;
+    	}
+
+    	if (currSqr == input) {
+    		range[0] = currGuess;
+    		range[1] = currGuess;
+    	} else {
+        	range[0] = min;
+        	range[1] = max;
+    	}
+    	return range;
+    	
+    }
+    	
+
+
 
 }
