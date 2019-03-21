28661cd919a0d5b857ca40522cf73a286f29c114
groiq <35578027+groiq@users.noreply.github.com>
2019-03-21 17:40:18 +0100

    set up checkGuess

--- src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGameMaster.java
+++ src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGameMaster.java
@@ -1,9 +1,32 @@
 package com.hackerrank.exercises.algorithms.sherlockAndSquares.simpleBinarySearch;
 
 public class BinarySearchGameMaster {
+	
+	private static int currWanted;
+	private static int upperRange;
+	
+	
+	
+	public static int checkGuess(int guess) {
+		
+		guess -= currWanted;
+		if(guess > 0) {
+			guess = 1;
+		} else if (guess < 0) {
+			guess = -1;
+		}
+		
+		return guess;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// test checkGuess()
+		for (currWanted = -2; currWanted < 3; currWanted++) {
+			for (int i = -2; i < 3; i++) {
+				System.out.println("currWanted: " + currWanted + ", guess: " + i + ", result: " + checkGuess(i));
+			}
+		}
 
 	}
 
