6b3fd67aeafba4d6e919222f5114f7105596b53f
groiq <35578027+groiq@users.noreply.github.com>
2019-03-22 21:43:23 +0100

    debug output

--- src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGuesser.java
+++ src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGuesser.java
@@ -12,13 +12,17 @@ public class BinarySearchGuesser {
 		
 		do {
 			currGuess = (min + max) / 2;
+			System.out.println("new guess: " + currGuess);
 			if (currGuess == min || currGuess == max) {
+				System.out.println("reached stop point");
 				currGuess = min * (-1);
 				break;
 			}
 			if (BinarySearchGameMaster.checkGuess(currGuess) == 1) {
+				System.out.println("too high");
 				min = currGuess;
 			} else { // should trigger when currGuess == 0, but shouldn't matter since the loop shouldn't be re-entered then
+				System.out.println("too low");
 				max = currGuess;
 			}
 		} while (BinarySearchGameMaster.checkGuess(currGuess) != 0);
