f84ab4c14396e59d9da3b8cd8c88ed84b1e125fe
groiq <35578027+groiq@users.noreply.github.com>
2019-03-22 21:41:26 +0100

    first attempt, throws false results

--- src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGuesser.java
+++ src/com/hackerrank/exercises/algorithms/sherlockAndSquares/simpleBinarySearch/BinarySearchGuesser.java
@@ -6,11 +6,23 @@ public class BinarySearchGuesser {
 		
 		int min = 1;
 		int max = BinarySearchGameMaster.getUpperRange();
+		int currGuess;
 		
 //		System.out.println("Playing game! Range is from " + min + " to " + max + ".");
 		
-		
-		
+		do {
+			currGuess = (min + max) / 2;
+			if (currGuess == min || currGuess == max) {
+				currGuess = min * (-1);
+				break;
+			}
+			if (BinarySearchGameMaster.checkGuess(currGuess) == 1) {
+				min = currGuess;
+			} else { // should trigger when currGuess == 0, but shouldn't matter since the loop shouldn't be re-entered then
+				max = currGuess;
+			}
+		} while (BinarySearchGameMaster.checkGuess(currGuess) != 0);
+		System.out.println("result is " + currGuess);
 	}
 	
 }
