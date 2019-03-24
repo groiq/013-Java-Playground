c9583a8ce3b26b6ae01a71c7d19c56dc9afef2ea
groiq <35578027+groiq@users.noreply.github.com>
2019-03-24 12:04:33 +0100

    writing code for testing the test code...

--- src/com/hackerrank/exercises/algorithms/ex04sherlockAndSquares/testCaseInPython/sherlockAndSquares.py
+++ src/com/hackerrank/exercises/algorithms/ex04sherlockAndSquares/testCaseInPython/sherlockAndSquares.py
@@ -5,16 +5,23 @@ def squares(a,b):
 
     def sqrt(number):
         
-        if number = 1: return 1
+        if number == 1: return 1
         
         min = 0
         max = number
-        currGuess = 1
-        currSquare = 1
+        guess = (max + min) // 2
+        square = guess * guess
         
+        while square != number:
+            if (guess == min | guess == max):
+                guess *= -1
+                break
+            if square > number:
+                max = guess
+            elif square < number:
+                min = guess
         
-        
-        return 0
+        return guess
         
     sqrtA = sqrt(a)
     sqrtB = sqrt(b)
@@ -23,57 +30,64 @@ def squares(a,b):
     
     return result
 
-            
+
+####################################
+    
+outfile = open("./outfile.txt","w")
+
     
 
 ##########################
+##
+##testIn = open("./input02.txt","r")
+##testOut = open("./output02.txt","r")
+##
+### bigList = list()
+##begins = list()
+##ends = list()
+##solutions = list()
+##
+##counter = 0
+##
+##for line in testIn:
+##
+##
+##    line = line.strip()
+##        
+##    line = line.split(" ")
+##    # outfile.write("{}\n".format(line))
+##    # print(line)
+##    # for item in line:
+##        # outfile.write(item + ",")
+##    # outfile.write("\n")
+##    
+##    begins.append(int(line[0]))
+##    ends.append((int(line[1])))
+##    
+##    counter += 1
+##    # if counter > 5:
+##        # break
+##    
+##for line in testOut:
+##    # outfile.write(line)
+##    solutions.append(int(line))
+##    # outfile.write("\n")
+## 
+##testIn.close()
+##testOut.close()
 
-testIn = open("./input02.txt","r")
-testOut = open("./output02.txt","r")
-
-outfile = open("./outfile.txt","w")
-
-# bigList = list()
-begins = list()
-ends = list()
-solutions = list()
-
-counter = 0
-
-for line in testIn:
-
-
-    line = line.strip()
-        
-    line = line.split(" ")
-    # outfile.write("{}\n".format(line))
-    # print(line)
-    # for item in line:
-        # outfile.write(item + ",")
-    # outfile.write("\n")
-    
-    begins.append(int(line[0]))
-    ends.append((int(line[1])))
-    
-    counter += 1
-    # if counter > 5:
-        # break
-    
-for line in testOut:
-    # outfile.write(line)
-    solutions.append(int(line))
-    # outfile.write("\n")
- 
-testIn.close()
-testOut.close()
+###############################
+# test the function
 
 # for i in range(12):
     # j = sqrtRange(i)
     # outfile.write("{} -> {}\n".format(i,j))
-for upper in range(10):
+for upper in range(1,5):
     for lower in range(upper):
-        k = squares(lower,upper)
-        outfile.write("range from {} to {}: {} squares\n".format(lower,upper,k))
+        j = lower + 1
+        k = upper - (j)
+        #k = squares(lower+1,upper)
+        #outfile.write("range from {} to {}: {} squares\n".format(lower,upper,k))
 
 # for i in range(5):
     # outfile.write("{},{} -> {} -> {}\n".format(begins[i],ends[i],solutions[i],squares(begins[i],ends[i])))
