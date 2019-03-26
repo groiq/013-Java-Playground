cce850d98119d380d69a421a24e70205dca9595d
groiq <35578027+groiq@users.noreply.github.com>
2019-03-26 10:27:36 +0100

    set up library fine exercise

---
+++ src/com/hackerrank/exercises/algorithms/ex05LibraryFine/LibraryFine.java
@@ -0,0 +1,14 @@
+/**
+ * https://www.hackerrank.com/challenges/library-fine/problem
+ */
+package com.hackerrank.exercises.algorithms.ex05LibraryFine;
+
+public class LibraryFine {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
