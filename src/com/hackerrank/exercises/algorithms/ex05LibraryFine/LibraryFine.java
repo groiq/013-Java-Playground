65d880926d47ebc3e8030e9f6930fc911fe9de54
groiq <35578027+groiq@users.noreply.github.com>
2019-03-26 10:32:29 +0100

    set up method and testing

--- src/com/hackerrank/exercises/algorithms/ex05LibraryFine/LibraryFine.java
+++ src/com/hackerrank/exercises/algorithms/ex05LibraryFine/LibraryFine.java
@@ -4,8 +4,23 @@
 package com.hackerrank.exercises.algorithms.ex05LibraryFine;
 
 public class LibraryFine {
+	
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+
+    	int fine = 0;
+    	
+    	
+    	
+    	return fine;
+
+    }
 
 	public static void main(String[] args) {
+		
+		System.out.println(libraryFine(30,10,2018,30,10,2018));
+		System.out.println(libraryFine(30,10,2018,20,10,2018));
+		System.out.println(libraryFine(30,10,2018,20,5,2018));
+		System.out.println(libraryFine(30,10,2018,20,10,2017));
 		
 		
 
