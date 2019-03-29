742c61fb86890b22a7729c937738ec595e9fa5de
groiq <35578027+groiq@users.noreply.github.com>
2019-03-29 09:24:50 +0100

    repeatedString: first attempt

--- src/com/hackerrank/exercises/algorithms/ex08RepeatedString/RepeatedString.java
+++ src/com/hackerrank/exercises/algorithms/ex08RepeatedString/RepeatedString.java
@@ -3,8 +3,25 @@ package com.hackerrank.exercises.algorithms.ex08RepeatedString;
 public class RepeatedString {
 	
 	static long repeatedString(String s, long n) {
+		
+		long remainder = n % s.length();
+		long occurrencesPartialString = 0;
+		long occurrencesWholeString = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a') {
+				occurrencesWholeString++;
+				if (i < remainder) {
+					occurrencesPartialString++;
+				}
+			}
+		}
+		
+		long wholeRepetitions = n / s.length();
+		
+		long result = (wholeRepetitions * occurrencesWholeString) + occurrencesPartialString; 
 
-		return n;
+		return result;
 
 	}
 
@@ -13,11 +30,12 @@ public class RepeatedString {
 		
 		String theString;
 		theString = "abba";
-		theString = "abc";
+//		theString = "abc";
+		theString = "a1a2a3";
 		
-		for (int i = 0; i < (theString.length()*3); i++) {
+		for (int i = 0; i < (theString.length()*3+1); i++) {
 			
-			System.out.println(stringRep(theString,i) + " -> " + repeatedString(theString, i));
+			System.out.println(i + " -> " + stringRep(theString,i) + " -> " + repeatedString(theString, i));
 			
 		}
 
@@ -27,7 +45,7 @@ public class RepeatedString {
 	private static String stringRep(String theString, int i) {
 		int index;
 		String result = "";
-		for (int j = 0; j < i+1; j++) {
+		for (int j = 0; j < i; j++) {
 			index = j % theString.length();
 			result += theString.charAt(index);
 		}
