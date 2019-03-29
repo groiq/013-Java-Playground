62c95e8d1d7e7b89a75f1aa725d9e585b17468b9
groiq <35578027+groiq@users.noreply.github.com>
2019-03-29 19:05:56 +0100

    implemented hashmap counter

--- src/com/hackerrank/exercises/algorithms/ex10EqualizeArray/EqualizeArray.java
+++ src/com/hackerrank/exercises/algorithms/ex10EqualizeArray/EqualizeArray.java
@@ -1,11 +1,19 @@
 package com.hackerrank.exercises.algorithms.ex10EqualizeArray;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class EqualizeArray {
 	
     static int equalizeArray(int[] arr) {
 
+    	HashMap<Integer,Integer> counter = new HashMap<Integer,Integer>();
+    	// variant with two int[] ?
+    	
+    	for (int i : arr) {
+    		counter.put(i, counter.getOrDefault(i, 0)+1);
+		}
+    	System.out.println(counter);
     	
     	return 0;
 
@@ -15,6 +23,7 @@ public class EqualizeArray {
 		
 		p(new int[] {1,2,2});
 		p(new int[] {1,2,2,3,3,3});
+		p(new int[] {1,1,1,2,2,3});
 
 	}
 	
