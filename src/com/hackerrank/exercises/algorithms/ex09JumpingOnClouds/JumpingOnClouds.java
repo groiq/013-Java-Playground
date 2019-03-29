a13582a0222f6b5c9472b6586b0b8e01cc07f57f
groiq <35578027+groiq@users.noreply.github.com>
2019-03-29 15:15:26 +0100

    exercise JumpingOnClouds: set up class

---
+++ src/com/hackerrank/exercises/algorithms/ex09JumpingOnClouds/JumpingOnClouds.java
@@ -0,0 +1,25 @@
+package com.hackerrank.exercises.algorithms.ex09JumpingOnClouds;
+
+import java.util.Arrays;
+
+public class JumpingOnClouds {
+
+	static int jumpingOnClouds(int[] c) {
+
+		int counter = 0;
+
+		return counter;
+
+	}		
+
+	public static void main(String[] args) {
+
+		p(new int[] {});
+
+	}
+	
+	static void p(int[] arr) {
+		System.out.println(Arrays.toString(arr) + " -> " + jumpingOnClouds(arr));
+	}
+
+}
