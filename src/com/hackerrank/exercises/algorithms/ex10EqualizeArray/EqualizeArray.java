a59d3bbfc9d00f6df6087d1ca3031d78b71cb1dc
groiq <35578027+groiq@users.noreply.github.com>
2019-03-29 18:54:41 +0100

    exercise equalizeArray: setup

---
+++ src/com/hackerrank/exercises/algorithms/ex10EqualizeArray/EqualizeArray.java
@@ -0,0 +1,25 @@
+package com.hackerrank.exercises.algorithms.ex10EqualizeArray;
+
+import java.util.Arrays;
+
+public class EqualizeArray {
+	
+    static int equalizeArray(int[] arr) {
+
+    	
+    	return 0;
+
+    }
+
+	public static void main(String[] args) {
+		
+		p(new int[] {1,2,2});
+		p(new int[] {1,2,2,3,3,3});
+
+	}
+	
+	static void p(int[] arr) {
+		System.out.println(Arrays.toString(arr) + " -> " + equalizeArray(arr));
+	}
+
+}
