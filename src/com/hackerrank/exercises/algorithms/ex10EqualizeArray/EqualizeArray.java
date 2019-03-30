6aa59a02d1cc269963e8b07aed67c88b0a38fe1e
groiq <35578027+groiq@users.noreply.github.com>
2019-03-30 10:38:52 +0100

    finalized two-array version

--- src/com/hackerrank/exercises/algorithms/ex10EqualizeArray/EqualizeArray.java
+++ src/com/hackerrank/exercises/algorithms/ex10EqualizeArray/EqualizeArray.java
@@ -7,8 +7,32 @@ public class EqualizeArray {
 	
     static int equalizeArray(int[] arr) {
 
+    	int[] keys = new int[arr.length];
+    	int[] values = new int[arr.length];
+//    	System.out.println(Arrays.toString(keys));
+    	
+    	int dictionaryLength = 0;
+    	for (int i : arr) {
+			for (int j = 0; j < keys.length; j++) {
+				if (keys[j] == i || keys[j] == 0) {
+					if (keys[j] == 0) {
+						keys[j] = i;
+						dictionaryLength++;
+					}
+					values[j]++;
+					break;
+				}
+			}
+		}
+//    	System.out.println(Arrays.toString(keys));
+//    	System.out.println(Arrays.toString(values));
+//    	System.out.println(dictionaryLength);
 
     	int result = 0;
+    	
+    	for (int i = 0; i < dictionaryLength; i++) {
+			if (values[i] > result) result = values[i];
+		}
     	
     	result = arr.length - result;
     	return result;
@@ -20,6 +44,7 @@ public class EqualizeArray {
 		p(new int[] {1,2,2});
 		p(new int[] {1,2,2,3,3,3});
 		p(new int[] {1,1,1,2,2,3});
+		p(new int[] {1,1,3});
 
 	}
 	
