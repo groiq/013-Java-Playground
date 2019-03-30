ff64917339ce52ae9e72d7f40772b12be45653d8
groiq <35578027+groiq@users.noreply.github.com>
2019-03-30 11:43:27 +0100

    ex. queens attack ii: setup

---
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -0,0 +1,20 @@
+package com.hackerrank.exercises.algorithms.ex11QueensAttackII;
+
+public class QueensAttack {
+	
+    static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
+
+    	
+    	
+    	return 0;
+
+    }
+
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
