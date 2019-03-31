35e85b016cfa82eba6ccc227a0dc50c470357774
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 11:40:53 +0200

    setting up loop...

--- src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -4,7 +4,17 @@ public class QueensAttack {
 	
 	static int queensAttackHelper(int boardSize, int obstacleCount, int queenX, int queenY, int[][] obstacles) {
 		
+		int offset = queenY - queenX;
 		
+		for (int[] obstacle : obstacles) {
+			if (obstacle[0] == queenX) {
+				
+			} if (obstacle[1] == queenY) {
+				
+			} if (obstacle[1] - obstacle[0] == offset) {
+				
+			}
+		}
 		
 		return 0;
 	}
@@ -16,7 +26,10 @@ public class QueensAttack {
 
 	public static void main(String[] args) {
 		
+		int[][] tester;
+		tester = new int[][] {new int[]{5,5},new int[] {4,2}, new int[] {2,3}};
 		
+		System.out.println(queensAttack(5, 3, 4, 3, tester));
 
 	}
 
