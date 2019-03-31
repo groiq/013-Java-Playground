f11743929bad88fcb6dd501815c180b12f7c9697
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 21:02:29 +0200

    finalized draft

--- src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
+++ src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
@@ -6,13 +6,45 @@ public class AcmIcpcTeam {
 	
     static int[] acmTeam(String[] topic) {
 
-    	for (String candidate : topic) {
-			int candidateInt = Integer.parseInt(candidate,2);
-			System.out.println(candidate + " -> " + candidateInt);
-		}
+    	short[] asNums = new short[topic.length];
+    	int[] counter = new int[topic[0].length()+1];
     	
+    	int runCounter = 0;
+    	for (int i = 0; i < asNums.length; i++) {
+//    		runCounter++;
+			asNums[i] = (short) Integer.parseInt(topic[i],2);
+//			System.out.println(topic[i] + " -> " + asNums[i]);
+			for (int j = 0; j < i; j++) {
+				runCounter++;
+				int k = asNums[i] | asNums[j];
+//				System.out.println("i: " + i + ", j: " + j + ", topic[i]: " + topic[i] + ", topic[j]: " + topic[j] + ", asNums[i]: " + asNums[i] + ", asNums[j]: " + asNums[j] + ", k: " + k);
+				System.out.print(runCounter + ": ");
+				System.out.println(topic[i] + ", " + topic[j] + " -> " + Integer.toBinaryString(k));
+				int topicCount = 0;
+				while (k > 0) {
+					// for later: do the same thing with bitwise operations
+					topicCount += (k%2);
+					k /= 2;
+//					System.out.println("k: " + k + ", topicCount: " + topicCount);
+				}
+//				System.out.println("final topic count: " + topicCount);
+				counter[topicCount] += 1;
+				System.out.println("topic count: " + topicCount + " -> " + Arrays.toString(counter));
+			}
+		}
+    	System.out.println(Arrays.toString(counter));
+    	
+    	int[] result = new int[2];
+    	for (int i = counter.length; i > 0; i--) {
+    		if (counter[i-1] > 0) {
+    			System.out.println("found: " + (i-1) + " -> " + counter[i-1]);
+    			result[0] = i-1;
+    			result[1] = counter[i-1];
+    			break;
+    		}
+    	}
 
-    	return null;
+    	return result;
     }
 
 	public static void main(String[] args) {
