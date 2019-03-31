031f6cb3269de585d5f1db776d5afb65e1c98514
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 11:24:41 +0200

    wrote a helperfunction with some better var names

--- src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -2,12 +2,15 @@ package com.hackerrank.exercises.algorithms.ex11QueensAttackII;
 
 public class QueensAttack {
 	
+	static int queensAttackHelper(int boardSize, int obstacleCount, int queenX, int queenY, int[][] obstacles) {
+		
+		
+		
+		return 0;
+	}
+	
     static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
-
-    	
-    	
-    	return 0;
-
+    	return queensAttackHelper(n, k, r_q, c_q, obstacles);
     }
 
 
