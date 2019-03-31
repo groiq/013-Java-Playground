245a2c47f18ea3c97137849d3f1711f792e15daa
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 12:26:09 +0200

    limit vars and conditions are in place

--- src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -1,18 +1,40 @@
 package com.hackerrank.exercises.algorithms.ex11QueensAttackII;
 
+import java.util.Arrays;
+
 public class QueensAttack {
 	
 	static int queensAttackHelper(int boardSize, int obstacleCount, int queenX, int queenY, int[][] obstacles) {
 		
-		int offset = queenY - queenX;
+		int risingD = queenY - queenX;
+		int fallingD = queenY + queenX;
+		
+		int horizHigh = boardSize+1;
+		int horizLow = 0;
+		int vertHigh = boardSize+1;
+		int vertLow = 0;
+		
+		int risingHigh = boardSize+1;
+		int risingLow = 0;
+		int fallingHigh = boardSize+1;
+		int fallingLow = 0;
 		
 		for (int[] obstacle : obstacles) {
+			System.out.println(Arrays.toString(obstacle));
 			if (obstacle[0] == queenX) {
-				
+				System.out.println("vertical");
+				if (obstacle[1] < queenX) {
+					vertLow = Math.max(vertLow, obstacle[1]);
+				} else {
+					vertHigh = Math.min(vertHigh, obstacle[1]);
+				}
 			} if (obstacle[1] == queenY) {
+				System.out.println("horizontal");
 				
-			} if (obstacle[1] - obstacle[0] == offset) {
-				
+			} if (obstacle[1] - obstacle[0] == risingD) {
+				System.out.println("rising");
+			} if (obstacle[0] + obstacle[1] == fallingD) {
+				System.out.println("falling");
 			}
 		}
 		
