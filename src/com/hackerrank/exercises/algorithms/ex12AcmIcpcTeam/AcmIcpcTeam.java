57ee21d29d53bd4a899b339f9e88fb38933362db
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 21:08:27 +0200

    test with sample input

--- src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
+++ src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
@@ -9,17 +9,17 @@ public class AcmIcpcTeam {
     	short[] asNums = new short[topic.length];
     	int[] counter = new int[topic[0].length()+1];
     	
-    	int runCounter = 0;
+//    	int runCounter = 0;
     	for (int i = 0; i < asNums.length; i++) {
 //    		runCounter++;
 			asNums[i] = (short) Integer.parseInt(topic[i],2);
 //			System.out.println(topic[i] + " -> " + asNums[i]);
 			for (int j = 0; j < i; j++) {
-				runCounter++;
+//				runCounter++;
 				int k = asNums[i] | asNums[j];
 //				System.out.println("i: " + i + ", j: " + j + ", topic[i]: " + topic[i] + ", topic[j]: " + topic[j] + ", asNums[i]: " + asNums[i] + ", asNums[j]: " + asNums[j] + ", k: " + k);
-				System.out.print(runCounter + ": ");
-				System.out.println(topic[i] + ", " + topic[j] + " -> " + Integer.toBinaryString(k));
+//				System.out.print(runCounter + ": ");
+//				System.out.println(topic[i] + ", " + topic[j] + " -> " + Integer.toBinaryString(k));
 				int topicCount = 0;
 				while (k > 0) {
 					// for later: do the same thing with bitwise operations
@@ -29,15 +29,15 @@ public class AcmIcpcTeam {
 				}
 //				System.out.println("final topic count: " + topicCount);
 				counter[topicCount] += 1;
-				System.out.println("topic count: " + topicCount + " -> " + Arrays.toString(counter));
+//				System.out.println("topic count: " + topicCount + " -> " + Arrays.toString(counter));
 			}
 		}
-    	System.out.println(Arrays.toString(counter));
+//    	System.out.println(Arrays.toString(counter));
     	
     	int[] result = new int[2];
     	for (int i = counter.length; i > 0; i--) {
     		if (counter[i-1] > 0) {
-    			System.out.println("found: " + (i-1) + " -> " + counter[i-1]);
+//    			System.out.println("found: " + (i-1) + " -> " + counter[i-1]);
     			result[0] = i-1;
     			result[1] = counter[i-1];
     			break;
@@ -51,7 +51,8 @@ public class AcmIcpcTeam {
 
 		String[] testData;
 //		testData = new String[] {"1100","1000","0010","0001"};
-		testData = new String[] {"000","001","010","011","100","101","110","111"};
+//		testData = new String[] {"000","001","010","011","100","101","110","111"};
+		testData = new String[] {"10101","11100","11010","00101"};
 		System.out.println(Arrays.toString(testData) + " -> " + Arrays.toString(acmTeam(testData)));
 		
 
