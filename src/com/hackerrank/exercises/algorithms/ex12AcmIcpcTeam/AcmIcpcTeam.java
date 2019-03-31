641ebd108bcddcb3018f376dd03af1112cb01183
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 20:11:23 +0200

    set up exercise 12: acm icpc teams

---
+++ src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
@@ -0,0 +1,20 @@
+package com.hackerrank.exercises.algorithms.ex12AcmIcpcTeam;
+
+public class AcmIcpcTeam {
+	
+    static int[] acmTeam(String[] topic) {
+
+    	
+
+    	return null;
+    }
+
+	public static void main(String[] args) {
+
+		String[] testData;
+		testData = new String[] {"1100","1000","0010","0001"};
+		
+
+	}
+
+}
