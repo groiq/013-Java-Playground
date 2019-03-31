24663446303fe33ac600bcc34679a706f72b955e
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 17:17:14 +0200

    horizontal and vertical calculated

--- src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -6,15 +6,26 @@ public class QueensAttack {
 	
 	static int queensAttackHelper(int boardSize, int obstacleCount, int queenX, int queenY, int[][] obstacles) {
 		
+		System.out.println(boardSize + "x" + boardSize + "," + queenX + "," + queenY);
+		
+		int maxHigh = boardSize + 1;
+		
+		int horizLow = 0;
+		int horizHigh = maxHigh;
+		int vertLow = 0;
+		int vertHigh = maxHigh;
+		
 		int risingD = queenY - queenX;
 		int fallingD = queenY + queenX;
+		
+		System.out.println(risingD + "||" + fallingD);
 		
 		
 		
 		int result = 0;
 		
-//		result = result + horizHigh - horizLow - 1;
-//		result = result + vertHigh - vertLow - 1;
+		result = result + horizHigh - horizLow - 2;
+		result = result + vertHigh - vertLow - 2;
 //		result = result + risingHigh - risingLow - 1;
 //		result = result + fallingHigh - fallingLow - 1;
 		
@@ -33,9 +44,9 @@ public class QueensAttack {
 		int[][] empty = new int[][] {new int[] {}};
 		
 //		System.out.println(queensAttack(5, 3, 4, 3, tester));
-		System.out.println(queensAttack(7, 0, 4, 2, empty));
+		System.out.println(queensAttack(7, 0, 3, 2, empty));
 		System.out.println(queensAttack(7, 0, 4, 4, empty));
-		System.out.println(queensAttack(7, 0, 4, 6, empty));
+		System.out.println(queensAttack(7, 0, 5, 6, empty));
 		
 
 	}
