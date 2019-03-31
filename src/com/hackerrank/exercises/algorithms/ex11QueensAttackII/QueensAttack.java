6db1003314994ea4904d8e622f4eb915b55dd6f1
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 16:58:23 +0200

    reboot with obstacle-less board

--- src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
+++ src/com/hackerrank/exercises/algorithms/ex11QueensAttackII/QueensAttack.java
@@ -9,60 +9,14 @@ public class QueensAttack {
 		int risingD = queenY - queenX;
 		int fallingD = queenY + queenX;
 		
-		int horizHigh = boardSize+1;
-		int horizLow = 0;
-		int vertHigh = boardSize+1;
-		int vertLow = 0;
 		
-//		int risingHigh = boardSize+1;
-//		int risingLow = 0;
-//		int fallingHigh = boardSize+1;
-//		int fallingLow = 0;
-		int risingHigh = Math.min(boardSize, boardSize+risingD)+1;
-		int risingLow = Math.max(0, risingD);
-		int fallingHigh = Math.min(boardSize+1, fallingD);
-		int fallingLow = Math.max(0, fallingD-boardSize);
-		
-		for (int[] obstacle : obstacles) {
-			System.out.println(Arrays.toString(obstacle));
-			if (obstacle[0] == queenX) {
-				System.out.println("vertical");
-				if (obstacle[1] < queenY) {
-					vertLow = Math.max(vertLow, obstacle[1]);
-				} else {
-					vertHigh = Math.min(vertHigh, obstacle[1]);
-				}
-			} if (obstacle[1] == queenY) {
-				System.out.println("horizontal");
-				if (obstacle[0] < queenX) {
-					horizLow = Math.max(horizLow, obstacle[0]);
-				} else {
-					horizHigh = Math.min(horizHigh, obstacle[0]);
-				}
-				
-			} if (obstacle[1] - obstacle[0] == risingD) {
-				System.out.println("rising");
-				if (obstacle[0] < queenX) {
-					risingLow = Math.max(risingLow, obstacle[0]);
-				} else {
-					risingHigh = Math.min(risingHigh, obstacle[0]);
-				}
-			} if (obstacle[0] + obstacle[1] == fallingD) {
-				System.out.println("falling");
-				if (obstacle[0] < queenX) {
-					fallingLow = Math.max(fallingLow, obstacle[0]);
-				} else {
-					fallingHigh = Math.min(fallingHigh, obstacle[0]);
-				}
-			}
-		}
 		
 		int result = 0;
 		
-		result = result + horizHigh - horizLow - 1;
-		result = result + vertHigh - vertLow - 1;
-		result = result + risingHigh - risingLow - 1;
-		result = result + fallingHigh - fallingLow - 1;
+//		result = result + horizHigh - horizLow - 1;
+//		result = result + vertHigh - vertLow - 1;
+//		result = result + risingHigh - risingLow - 1;
+//		result = result + fallingHigh - fallingLow - 1;
 		
 		return result;
 	}
@@ -76,8 +30,13 @@ public class QueensAttack {
 		
 		int[][] tester;
 		tester = new int[][] {new int[]{5,5},new int[] {4,2}, new int[] {2,3}};
+		int[][] empty = new int[][] {new int[] {}};
 		
-		System.out.println(queensAttack(5, 3, 4, 3, tester));
+//		System.out.println(queensAttack(5, 3, 4, 3, tester));
+		System.out.println(queensAttack(7, 0, 4, 2, empty));
+		System.out.println(queensAttack(7, 0, 4, 4, empty));
+		System.out.println(queensAttack(7, 0, 4, 6, empty));
+		
 
 	}
 
