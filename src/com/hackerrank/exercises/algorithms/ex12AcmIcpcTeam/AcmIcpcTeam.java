b4d5002ffb05d7c6aea38594a75c809fec07e421
groiq <35578027+groiq@users.noreply.github.com>
2019-03-31 20:21:42 +0200

    parseInt works...

--- src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
+++ src/com/hackerrank/exercises/algorithms/ex12AcmIcpcTeam/AcmIcpcTeam.java
@@ -1,9 +1,15 @@
 package com.hackerrank.exercises.algorithms.ex12AcmIcpcTeam;
 
+import java.util.Arrays;
+
 public class AcmIcpcTeam {
 	
     static int[] acmTeam(String[] topic) {
 
+    	for (String candidate : topic) {
+			int candidateInt = Integer.parseInt(candidate,2);
+			System.out.println(candidate + " -> " + candidateInt);
+		}
     	
 
     	return null;
@@ -12,7 +18,9 @@ public class AcmIcpcTeam {
 	public static void main(String[] args) {
 
 		String[] testData;
-		testData = new String[] {"1100","1000","0010","0001"};
+//		testData = new String[] {"1100","1000","0010","0001"};
+		testData = new String[] {"000","001","010","011","100","101","110","111"};
+		System.out.println(Arrays.toString(testData) + " -> " + Arrays.toString(acmTeam(testData)));
 		
 
 	}
