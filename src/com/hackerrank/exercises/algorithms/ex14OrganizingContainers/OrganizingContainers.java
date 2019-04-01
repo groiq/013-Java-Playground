a54d87ef33de446f060d9db601588ce2c2547a64
groiq <35578027+groiq@users.noreply.github.com>
2019-04-01 15:35:05 +0200

    set up test cases

--- src/com/hackerrank/exercises/algorithms/ex14OrganizingContainers/OrganizingContainers.java
+++ src/com/hackerrank/exercises/algorithms/ex14OrganizingContainers/OrganizingContainers.java
@@ -4,8 +4,9 @@ public class OrganizingContainers {
 	
 	static boolean helper(int[][] container) {
 		
-		return (container[0][1] == container[1][0] || container[0][0] == container[1][1]);
 		
+		
+		return (container[0][1] == container[1][0] || container[0][0] == container[1][1]);
 		
 	}
 	
@@ -17,7 +18,27 @@ public class OrganizingContainers {
 
 	public static void main(String[] args) {
 		
-		
+		int[][] i00 = {{1,1},{1,1}};
+		int[][] i01 = {{0,2},{1,1}};
+		int[][] i10 = {{1,3,1},{2,1,2},{3,3,3}};
+		int[][] i11 = {{0,2,1},{1,1,1},{2,0,0}};
+		System.out.println(helper(i00));
+		System.out.println(helper(i01));
+		System.out.println(helper(i10));
+		System.out.println(helper(i11));
+//		1 1
+//		1 1
+//		2
+//		0 2
+//		1 1
+//		
+//		1 3 1
+//		2 1 2
+//		3 3 3
+//		3
+//		0 2 1
+//		1 1 1
+//		2 0 0
 
 	}
 
