11f6510a88b1a8644332ee4bafadaea47c5f7b3f
groiq <35578027+groiq@users.noreply.github.com>
2019-04-02 08:06:30 +0200

    exercise encryption: set up package

---
+++ src/com/hackerrank/exercises/algorithms/ex15Encryption/Encryption.java
@@ -0,0 +1,10 @@
+package com.hackerrank.exercises.algorithms.ex15Encryption;
+
+public class Encryption {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
