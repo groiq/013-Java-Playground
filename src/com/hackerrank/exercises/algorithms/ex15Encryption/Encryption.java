61c8aacd39e0f4ceb0b83cd7bd3b124bae807b4c
groiq <35578027+groiq@users.noreply.github.com>
2019-04-02 08:09:15 +0200

    tested sqrt function

--- src/com/hackerrank/exercises/algorithms/ex15Encryption/Encryption.java
+++ src/com/hackerrank/exercises/algorithms/ex15Encryption/Encryption.java
@@ -3,7 +3,12 @@ package com.hackerrank.exercises.algorithms.ex15Encryption;
 public class Encryption {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 12; i++) {
+			int j = i+1;
+			j = (int) Math.sqrt(j);
+			System.out.println(i+1 + "->" + j);
+		}
 
 	}
 
