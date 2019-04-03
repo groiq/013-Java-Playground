b6a2e3e1c9612459d5ec10dd84f0d41cefd91e32
groiq <35578027+groiq@users.noreply.github.com>
2019-04-03 12:19:04 +0200

    exercise Bigger is Greater: setup

---
+++ src/com/hackerrank/exercises/algorithms/ex16BiggerIsGreater/BiggerIsGreater.java
@@ -0,0 +1,13 @@
+/*
+ * Idea: This is all about permutations. Try a permutation generator first? Didn't I have such a thing already?
+ */
+package com.hackerrank.exercises.algorithms.ex16BiggerIsGreater;
+
+public class BiggerIsGreater {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
