fc7006b2c3530d703432d8e5ea3005c03672f023
groiq <35578027+groiq@users.noreply.github.com>
2019-04-03 12:02:59 +0200

    did the whole thing again, with much help from the forum

--- src/com/hackerrank/exercises/algorithms/ex15Encryption/Encryption02.java
+++ src/com/hackerrank/exercises/algorithms/ex15Encryption/Encryption02.java
@@ -11,21 +11,28 @@ public class Encryption02 {
 			}
 			String theString = sb.toString();
 			System.out.println(theString);
-			System.out.println(encrypt(theString));
+			System.out.println(encryption(theString));
 		}
 
 	}
 
-	private static String encrypt(String s) {
-		
-		int len = s.length();
-		int rows = (int) Math.round(Math.sqrt(len));
-		int cols = (rows*rows < len ? rows+1 : rows);
-		
-		
-		return null;
-	}
-
+    private static String encryption(String s) {
+        
+        int len = s.length();
+        int rows = (int) Math.round(Math.sqrt(len));
+        int cols = (rows*rows < len ? rows+1 : rows);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int curRow = 0; curRow < cols; curRow++) {
+            for (int curIndex = curRow; curIndex < len; curIndex += cols) {
+                sb.append(s.charAt(curIndex));
+            }
+            if (curRow < rows) sb.append(' ');
+        }
+        
+        return sb.toString();
+    }
 	
 
 }
