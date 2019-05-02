dabd812b268461de5d4fd80b41f827cb6e861143
groiq <35578027+groiq@users.noreply.github.com>
2019-05-02 11:35:48 +0200

    exercise modified kaprekar numbers: setup

---
+++ src/com/hackerrank/exercises/algorithms/ex17ModifiedKaprekarNumbers/ModifiedKaprekarNumbers.java
@@ -0,0 +1,18 @@
+package com.hackerrank.exercises.algorithms.ex17ModifiedKaprekarNumbers;
+
+public class ModifiedKaprekarNumbers {
+	
+    static void kaprekarNumbers(int p, int q) {
+
+    	
+    	System.out.println("function goes here");
+
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		kaprekarNumbers(1,200);
+
+	}
+
+}
