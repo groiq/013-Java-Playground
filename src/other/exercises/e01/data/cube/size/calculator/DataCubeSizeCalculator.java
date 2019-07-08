1bd079b632eaac9970e9f3257405091f18b7a494
groiq <35578027+groiq@users.noreply.github.com>
2019-07-08 11:02:16 +0200

    Create DataCubeSizeCalculator.java

---
+++ src/other/exercises/e01/data/cube/size/calculator/DataCubeSizeCalculator.java
@@ -0,0 +1,23 @@
+package other.exercises.e01.data.cube.size.calculator;
+
+/**
+ * 
+ * @author HAL
+ *
+ * A question that just came up in my database course:
+ * Given a data cube with a known set of dimensions,
+ * and a known number of tuples per dimension,
+ * how many fields are there?
+ * https://lagunita.stanford.edu/courses/DB/OLAP/SelfPaced/courseware/ch-on-line_analytical_processing/seq-vid-olap/?activate_block_id=i4x%3A%2F%2FDB%2FOLAP%2Fsequential%2Fseq-vid-olap
+ * 
+ * Thought I'd write a calculator for that.
+ *
+ */
+public class DataCubeSizeCalculator {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
