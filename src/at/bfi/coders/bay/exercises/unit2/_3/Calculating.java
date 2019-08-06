cca51fdeaba6addbdb82e8885c46c672fe1a38aa
groiq <35578027+groiq@users.noreply.github.com>
2019-08-06 10:54:30 +0200

    exercise calculating

---
+++ src/at/bfi/coders/bay/exercises/unit2/_3/Calculating.java
@@ -0,0 +1,28 @@
+package at.bfi.coders.bay.exercises.unit2._3;
+
+/**
+ * 
+ * @author groiq
+ *
+ * Aufgabe: Deklaration von 2 int Variablen
+ * Initialisierung mit Werten
+ * Rechnen mit +,-,*,/ 
+ * Direkt in einer Systemausgabe den Wert ausgeben
+ *
+ */
+public class Calculating {
+
+	public static void main(String[] args) {
+		
+		int i1 = 6;
+		int i2 = 2;
+		
+		System.out.println(i1 + i2);
+		System.out.println(i1 - i2);
+		System.out.println(i1 * i2);
+		System.out.println(i1 / i2);
+		
+
+	}
+
+}
