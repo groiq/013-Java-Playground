ca4db7643ec0d7d2cc033f3921724cd085c1a52e
groiq <35578027+groiq@users.noreply.github.com>
2019-08-06 12:56:44 +0200

    set up Shopping List class

---
+++ src/at/bfi/coders/bay/exercises/unit2/_3/receipt/ShoppingList.java
@@ -0,0 +1,41 @@
+package at.bfi.coders.bay.exercises.unit2._3.receipt;
+
+public class ShoppingList {
+	
+	String[] goods;
+	int[] amounts;
+	
+	public String[] getGoods() {
+		return goods;
+	}
+	public void setGoods(String[] goods) {
+		this.goods = goods;
+	}
+	public int[] getAmounts() {
+		return amounts;
+	}
+	public void setAmounts(int[] amounts) {
+		this.amounts = amounts;
+	}
+	
+	public ShoppingList(Store theStore, int[] givenAmounts) {
+		this.goods = theStore.getGoods();
+		this.amounts = new int[goods.length];
+		
+		for (int i = 0; i < goods.length; i++) {
+			if (i < givenAmounts.length) {
+				this.amounts[i] = givenAmounts[i];
+			} else {
+				this.amounts[i] = 0;
+			}
+		}
+		
+	}
+	
+	
+
+	
+	
+	
+
+}
