f07c3cb5a39411f1edc44a53b4c3141572b76fd7
groiq <35578027+groiq@users.noreply.github.com>
2019-08-06 10:47:04 +0200

    clean up + comment

--- src/at/bfi/coders/bay/exercises/unit2/_2/DeclareVariables.java
+++ src/at/bfi/coders/bay/exercises/unit2/_2/DeclareVariables.java
@@ -2,13 +2,13 @@ package at.bfi.coders.bay.exercises.unit2._2;
 
 public class DeclareVariables {
 	
+	// Fields
 	boolean sampleBool;
 	int sampleInt;
 	String sampleString;
 	double sampleDouble;
 	
-	
-
+	// Constructor
 	public DeclareVariables(boolean sampleBool, int sampleInt, String sampleString, double sampleDouble) {
 		super();
 		this.sampleBool = sampleBool;
@@ -17,70 +17,46 @@ public class DeclareVariables {
 		this.sampleDouble = sampleDouble;
 	}
 
+	// Getters and Setters
 	
-
-
 	public boolean isSampleBool() {
 		return sampleBool;
 	}
-
-
-
 
 	public void setSampleBool(boolean sampleBool) {
 		this.sampleBool = sampleBool;
 	}
 
 
-
-
 	public int getSampleInt() {
 		return sampleInt;
 	}
-
-
-
 
 	public void setSampleInt(int sampleInt) {
 		this.sampleInt = sampleInt;
 	}
 
-
-
-
 	public String getSampleString() {
 		return sampleString;
 	}
-
-
-
-
-
 
 	public void setSampleString(String sampleString) {
 		this.sampleString = sampleString;
 	}
 
-
-
-
 	public double getSampleDouble() {
 		return sampleDouble;
 	}
-
-
-
 
 	public void setSampleDouble(double sampleDouble) {
 		this.sampleDouble = sampleDouble;
 	}
 
+	// toString
+	
 	@Override
 	public String toString() {
 		return ("a boolean: " + sampleBool + ", an int: " + sampleInt + ", a String: " + sampleString + ", a double: " + sampleDouble + ".");
 	}
-
-
-
 
 }
