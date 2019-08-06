a4c4db34c1d86b88a87ad527a3efae76772eceef
groiq <35578027+groiq@users.noreply.github.com>
2019-08-06 11:58:07 +0200

    set up a store class

---
+++ src/at/bfi/coders/bay/exercises/unit2/_3/receipt/Store.java
@@ -0,0 +1,34 @@
+package at.bfi.coders.bay.exercises.unit2._3.receipt;
+
+public class Store {
+	
+	String[] goods;
+	double[] prices;
+	
+	public String[] getGoods() {
+		return goods;
+	}
+	public void setGoods(String[] goods) {
+		this.goods = goods;
+	}
+	public double[] getPrices() {
+		return prices;
+	}
+	public void setPrices(double[] prices) {
+		this.prices = prices;
+	}
+	public Store(String[] goods, double[] prices) {
+		int size = Math.min(goods.length,prices.length);
+		
+		this.goods = new String[size];
+		this.prices = new double[size];
+		
+		for (int i = 0; i < size; i++) {
+			this.goods[i] = goods[i];
+			this.prices[i] = prices[i];
+		}
+	}
+	
+	
+
+}
