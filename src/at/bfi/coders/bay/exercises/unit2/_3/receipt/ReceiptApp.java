8a5ef051f6f3e73a366fffa1bca09910e84fe295
groiq <35578027+groiq@users.noreply.github.com>
2019-08-06 11:30:41 +0200

    Set up receipt exercise

---
+++ src/at/bfi/coders/bay/exercises/unit2/_3/receipt/ReceiptApp.java
@@ -0,0 +1,38 @@
+package at.bfi.coders.bay.exercises.unit2._3.receipt;
+
+/*
+ * Es soll ein Programm geschrieben werden, das einen Kassenbon auf der Konsole anzeigt (mit System.out.println). Am Anfang des Programmes werden in Form von Variablen folgende Werte festgelegt:
+
+Anzahl von mindestens vier verschiedenen Waren, die gekauft werden sollen.
+Einzelpreise der Waren. (Ja, jede Warensorte hat einen anderen Preis)
+Inhalt der Brieftasche.
+Das Programm soll den Gesamtpreis der eingangs festgelegten Waren ermitteln. Übersteigt der Gesamtpreis den Inhalt der Brieftasche, wird der Benutzer auf den fehlenden Betrag hingewiesen. Reicht der Inhalt der Brieftasche für den Einkauf aus, wird ein Kassenbon ausgegeben, der die gekauften Waren mit Anzahl und Preis sowie am Ende den Gesamtpreis auflistet.
+
+Hinweis: Erstellt zuerst die Ausgabe des Kassenbons und füllt sie anschließend mit Berechnungen und der Fallunterscheidung.
+
+
+Beispiel wie ein Kassenbon aussehen könnte:
+
+Wurst      1 x  4.20 EUR
+                          4.20 EUR
+Käse       1 x  2.30 EUR
+                          2.30 EUR
+Brot       1 x  2.10 EUR
+                          2.10 EUR
+DVD        2 x 12.00 EUR
+                         24.00 EUR
+----------------------------------
+Gesamt                   32.60 EUR
+Gegeben                  50.00 EUR
+
+Zurück                   17.40 EUR
+
+ */
+public class ReceiptApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
