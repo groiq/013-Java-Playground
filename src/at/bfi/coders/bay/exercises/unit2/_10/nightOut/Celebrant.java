98ccb7e1695d2e6127af4d3c5f8ac3e56c10f9ba
groiq <35578027+groiq@users.noreply.github.com>
2019-08-08 14:12:31 +0200

    set up exercise "night out"

---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/nightOut/Celebrant.java
@@ -0,0 +1,23 @@
+package at.bfi.coders.bay.exercises.unit2._10.nightOut;
+
+public class Celebrant {
+	
+	private int glassesDrunk;
+	private float curGlassContents;
+	
+	public int getGlassesDrunk() {
+		return glassesDrunk;
+	}
+	public void setGlassesDrunk(int glassesDrunk) {
+		this.glassesDrunk = glassesDrunk;
+	}
+	public float getCurGlassContents() {
+		return curGlassContents;
+	}
+	public void setCurGlassContents(float curGlassContents) {
+		this.curGlassContents = curGlassContents;
+	}
+	
+	
+
+}
---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/nightOut/NightOutApp.java
@@ -0,0 +1,30 @@
+package at.bfi.coders.bay.exercises.unit2._10.nightOut;
+
+/**
+ * 
+ * @author groiq
+ *
+ * Apparently all the tasks revolve around the actoins of some celebrating person.
+ * I better set up a class called "celebrant". Thus I can seperate the implementation from the checks.
+ * 
+ * 
+ *
+ */
+public class NightOutApp {
+
+	public static void main(String[] args) {
+		
+		/**
+		 *  Definiere eine Variable in der du ml abspeichern kannst (egal ob mit oder ohne Nachkommazahlen).
+		 *  Lege einen Wert fest und überprüfe mit einer geigneten Verzweigung, folgende Situation:
+		 *     Das Glas hat genau  250ml Inhalt --> Ausgabe "Das Glas ist voll"
+		 *     Das Glas hat weniger als 250ml Inhalt --> Ausgabe "Das Glas ist nicht mehr voll"
+		 *     Das Glas hat weniger als 20ml oder ist ganz leer --> Ausgabe "Kellner neues Bier*"
+		 *     *Statt Bier kann gerne auch Spritzer bestellt werden
+		 */
+		
+		
+
+	}
+
+}
