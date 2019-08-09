96a48b8f0362c908c5b03073b92f63da44de6f39
groiq <35578027+groiq@users.noreply.github.com>
2019-08-09 12:23:14 +0200

    other variant of pyramid
    different variant of the pyramid program. Bit shoddy in execution, but I guess I've got the basic principles.

--- src/at/bfi/coders/bay/exercises/unit2/_10/pyramid/Pyramid.java
+++ src/at/bfi/coders/bay/exercises/unit2/_10/pyramid/Pyramid.java
@@ -8,7 +8,8 @@ public class Pyramid {
 	public static void main(String[] args) {
 		
 		blank = "_";
-		blank = " ";
+		blank = "-";
+//		blank = " ";
 		sign = "X";
 		
 //		buildPyramid(4);
@@ -24,42 +25,43 @@ public class Pyramid {
 
 	}
 
-//	private static void buildPyramid(int lineCount) {
-//		for (int i = 0; i < lineCount; i++) {
-//			int curLine = i + 1;
-//			for (int j = -lineCount; j < lineCount + 1; j++) {
-////				System.out.print(j);
-//				if (Math.abs(j) <= curLine && j % 2 == 0) {
-//					System.out.print("X");
-//				} else {
-//					System.out.print("_");
-//				}
-//			}
-//			System.out.println();
-//		}
-//	}
-	
 	private static void buildPyramid(int lineCount) {
-//		int totalLen = (lineCount - 1) * 4 + 1;
-//		System.out.println(lineCount*4-3);
-		int totalLen = (lineCount*4)-3;
-//		System.out.println(totalLen);
-		for (int curLineNum = 1; curLineNum <= lineCount; curLineNum++) {
-			int curLineLen = curLineNum * 4 - 3;
-			int padding = (totalLen - curLineLen) / 2;
-			System.out.print(padLine(padding));
-			for (int i = 0; i < curLineLen; i++) {
-//				System.out.print(sign);
-				if (i % 2 == 0) {
+		int lineLen = lineCount * 2 - 2;
+		for (int i = 0; i < lineCount; i++) {
+			int curLine = i + 1;
+			for (int j = -lineLen; j < lineLen + 1; j++) {
+//				System.out.print(j);
+				if (Math.abs(j) < (curLine * 2) - 2 && j % 2 == 0) {
 					System.out.print(sign);
 				} else {
 					System.out.print(blank);
 				}
 			}
-			System.out.print(padLine(padding));
 			System.out.println();
 		}
 	}
+	
+//	private static void buildPyramid(int lineCount) {
+////		int totalLen = (lineCount - 1) * 4 + 1;
+////		System.out.println(lineCount*4-3);
+//		int totalLen = (lineCount*4)-3;
+////		System.out.println(totalLen);
+//		for (int curLineNum = 1; curLineNum <= lineCount; curLineNum++) {
+//			int curLineLen = curLineNum * 4 - 3;
+//			int padding = (totalLen - curLineLen) / 2;
+//			System.out.print(padLine(padding));
+//			for (int i = 0; i < curLineLen; i++) {
+////				System.out.print(sign);
+//				if (i % 2 == 0) {
+//					System.out.print(sign);
+//				} else {
+//					System.out.print(blank);
+//				}
+//			}
+//			System.out.print(padLine(padding));
+//			System.out.println();
+//		}
+//	}
 	
 	private static String padLine(int padding) {
 		String result = "";
