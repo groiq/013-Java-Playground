a9a8e2c77aa2fbb81fd1b6c85cd22f33015f2119
groiq <35578027+groiq@users.noreply.github.com>
2019-08-10 10:57:49 +0200

    set up calendar exercise

---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/calendar/Calendar.java
@@ -0,0 +1,12 @@
+package at.bfi.coders.bay.exercises.unit2._10.calendar;
+
+public class Calendar {
+	
+	public static String printCalendar(int weekdayOfFirst) {
+		String result;
+		result = "";
+		
+		return result;
+	}
+
+}
---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/calendar/CalendarTester.java
@@ -0,0 +1,20 @@
+package at.bfi.coders.bay.exercises.unit2._10.calendar;
+
+/*
+Aufgabe: Kalender (Advanced)
+
+Schreibe ein Programm, das für den Monat Jänner den Kalender ausgibt. 
+Die Darstellung soll ich einer Art Matrix erfolgen in der die erste Zeile MO|DI|MI|DO|FR|SA|SO
+Wenn beispielsweise der erste des Monats ein Mittwoch ist, dann müssen Montag und Dienstag freigelassen werden. Mittwoch soll dann 1 ausgegeben werden, Donnerstag 2.
+Alles wird mit einem | getrennt
+ *
+ */
+public class CalendarTester {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
