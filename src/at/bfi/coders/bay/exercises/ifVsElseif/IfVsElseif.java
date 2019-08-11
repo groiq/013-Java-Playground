e6087072c33b207eae7c5e5f450e2b58a519b06e
groiq <35578027+groiq@users.noreply.github.com>
2019-08-11 17:20:55 +0200

    if vs else if: code is up and running

--- src/at/bfi/coders/bay/exercises/ifVsElseif/IfVsElseif.java
+++ src/at/bfi/coders/bay/exercises/ifVsElseif/IfVsElseif.java
@@ -1,5 +1,10 @@
 package at.bfi.coders.bay.exercises.ifVsElseif;
 
+/**
+ * 
+ * @author groiq
+ *
+ */
 public class IfVsElseif {
 	
 	private static void coffeeBreak(boolean aliceIsThere, boolean bobIsThere) {
@@ -8,7 +13,38 @@ public class IfVsElseif {
 		System.out.println("<aliceIsThere: " + aliceIsThere + ", bobIsThere: " + bobIsThere + ">");
 		System.out.println("It's coffee break time!");
 		
+		if (aliceIsThere) {
+			System.out.println("Alice is making coffee.");
+		} else if (bobIsThere) {
+			System.out.println("Bob is making coffee.");
+		} else {
+			System.out.println("Nobody's here, so nobody's making any coffee.");
+		}
 		
+		if (aliceIsThere) {
+			System.out.println("Alice is having some coffee.");
+		}
+		if (bobIsThere) {
+			System.out.println("Bob is having some coffee.");
+		}
+		
+		if (aliceIsThere) {
+			System.out.println("Alice brought cookies.");
+			if (bobIsThere) {
+				System.out.println("Bob is having some cookies.");
+			}
+		}
+		
+		if (bobIsThere) {
+			System.out.println("Bob is loading the dishwasher.");
+		} else {
+			if (aliceIsThere) {
+				System.out.println("Alice is loading the dishwasher.");
+			} else {
+				System.out.println("Since there's nobody here, there's no dirty cups either.");
+			}
+		}
+
 		
 		
 		System.out.println("-----------------------------------");
