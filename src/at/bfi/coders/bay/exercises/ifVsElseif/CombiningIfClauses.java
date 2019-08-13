d93763da1f148d3089faf6fd9ea2d231153c2c54
groiq <35578027+groiq@users.noreply.github.com>
2019-08-13 10:08:55 +0200

    combining if clauses: rearranged main method

--- src/at/bfi/coders/bay/exercises/ifVsElseif/CombiningIfClauses.java
+++ src/at/bfi/coders/bay/exercises/ifVsElseif/CombiningIfClauses.java
@@ -8,17 +8,37 @@ package at.bfi.coders.bay.exercises.ifVsElseif;
  */
 public class CombiningIfClauses {
 
-	/*
-	 * In my example I assume that Alice and Bob are two coworkers who often have coffee break together.
-	 * Right now it's coffee break time, and what's happening in the break room depends on
-	 * (a) whether Alice is having a break right now and 
-	 * (b) whether Bob is having a break right now.
-	 * 
-	 * I am defining two variables named aliceIsThere and bobIsThere. These are both boolean,
-	 * meaning they can be true or false.
-	 * 
-	 * I will then write a few code blocks, each of which checks for both variables in different combinations.
-	 */
+	public static void main(String[] args) {
+		
+		/*
+		 * An example for various ways of combining two if clauses.
+		 * Let Alice and Bob be two coworkers who often have coffee break together.
+		 * Right now it's coffee break time, and what's happening in the break room depends on
+		 * (a) whether Alice is having a break right now and 
+		 * (b) whether Bob is having a break right now.
+		 * 
+		 * I am using two variables named aliceIsThere and bobIsThere.
+		 * These two are boolean, meaning that the possible values are true and false.
+		 * 
+		 * I will then write a few code blocks, each of which checks for both variables in different combinations.
+		 * 
+		 * I will run the code four times.
+		 * 
+		 * First we assume that neither Alice nor Bob are there,
+		 * then that only Bob is there,
+		 * then that only Alice is there,
+		 * then that both are there.
+		 * 
+		 * Of course I don't want to write the same code four times, so I'll throw it all into a method.
+		 */
+		coffeeBreak(false,false);
+		coffeeBreak(false,true);
+		coffeeBreak(true,false);
+		coffeeBreak(true,true);
+		
+
+	}
+
 	private static void coffeeBreak(boolean aliceIsThere, boolean bobIsThere) {
 
 		/*
@@ -108,21 +128,5 @@ public class CombiningIfClauses {
 
 	}
 	
-	/*
-	 * Now we run that code four times.
-	 * 
-	 * First we assume that neither Alice nor Bob are there,
-	 * then that only Bob is there,
-	 * then that only Alice is there,
-	 * then that both are there.
-	 */
-	public static void main(String[] args) {
-		
-		coffeeBreak(false,false);
-		coffeeBreak(false,true);
-		coffeeBreak(true,false);
-		coffeeBreak(true,true);
-
-	}
 
 }
