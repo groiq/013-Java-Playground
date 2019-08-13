779a4935dd29cc360f74b1760f738fc6de6e7375
groiq <35578027+groiq@users.noreply.github.com>
2019-08-13 09:28:13 +0200

    combining if clauses: finished coffee break example

--- src/at/bfi/coders/bay/exercises/ifVsElseif/CombiningIfClauses.java
+++ src/at/bfi/coders/bay/exercises/ifVsElseif/CombiningIfClauses.java
@@ -21,23 +21,90 @@ public class CombiningIfClauses {
 	 */
 	private static void coffeeBreak(boolean aliceIsThere, boolean bobIsThere) {
 
+		/*
+		 * let's start with a bit of debug output...
+		 */
+		
+		System.out.println("<method called with these parameters:>");
+		System.out.println("<aliceIsThere: " + aliceIsThere + ", bobIsThere: " + bobIsThere + ">");
+		System.out.println();
+
+		
 		/* 
-		 * who's having coffee - independent if-clauses
+		 * First, I'll check who is actually having a coffee break.
+		 * First I check the Alice variable, 
+		 * and then I check the Bob variable *independent of* what the first check returned.
 		 * 
 		 */
 		
-		/* 
-		 * who's having cookies - second if clause is only checked if first one is true
-		 */
-		
-		/*
-		 * who's making coffee - second if clause is only checked if first one is false
-		 */
-		
-		/*
-		 * who's loading the dishwasher - same with if-else
-		 */
+		System.out.println("It's coffee break time!");
 
+		if (aliceIsThere) {
+			System.out.println("Alice is having a break.");
+		}
+		
+		if (bobIsThere) {
+			System.out.println("Bob is having a break.");
+		}
+		
+		System.out.println();
+		
+		/* 
+		 * Next, *if* Alice is there, she brought some cookies along.
+		 * If Bob is there *too*, he'll have some.
+		 * Bob's variable is checked *only* if Alice's check returned true.
+		 */
+		if (aliceIsThere) {
+			System.out.println("Alice brought cookies.");
+			if (bobIsThere) {
+				System.out.println("Bob is having some cookies.");
+			}
+		}
+		
+		System.out.println();
+
+		
+		/*
+		 * Now to decide who's making the coffee.
+		 * If Alice is here, she volunteers to make coffee, so Bob doesn't have to.
+		 * If Bob is here and Alice isn't, he'll make the coffee.
+		 * So Bob's variable is checked only if Alice's returned false.
+		 */
+		
+		if (aliceIsThere) {
+			System.out.println("Alice is making coffee.");
+		} else {
+			if (bobIsThere) {
+				System.out.println("Bob is making coffee.");
+			} else {
+				System.out.println("Nobody's here, so nobody's making any coffee.");
+			}
+		}
+		System.out.println();
+		
+		
+		/*
+		 * After the break somebody needs to clean up and load the dishwasher.
+		 * Alice insisted that this time we check for Bob first. If he isn't there, we'll check for Alice.
+		 * So the code is equivalent to the previous one, but this time we'll use an if-else clause.
+		 */
+		
+		if (bobIsThere) {
+			System.out.println("Bob is loading the dishwasher.");
+		} else if (aliceIsThere) {
+			System.out.println("Alice is loading the dishwasher.");
+		} else {
+			System.out.println("Since there's nobody here, there's no dirty cups either.");
+		}
+		
+		System.out.println();
+
+		/*
+		 * I'll wrap up the output with a delimiter...
+		 */
+		
+		System.out.println("-----------------------------------");
+		System.out.println();
 
 	}
 	
@@ -55,22 +122,6 @@ public class CombiningIfClauses {
 		coffeeBreak(false,true);
 		coffeeBreak(true,false);
 		coffeeBreak(true,true);
-		/* 
-		 * who's having coffee - independent if-clauses
-		 * 
-		 */
-		
-		/* 
-		 * who's having cookies - second if clause is only checked if first one is true
-		 */
-		
-		/*
-		 * who's making coffee - second if clause is only checked if first one is false
-		 */
-		
-		/*
-		 * who's loading the dishwasher - same with if-else
-		 */
 
 	}
 
