2cb09f4e48aa03d2e9139e56dd19b80a8dcc0872
groiq <35578027+groiq@users.noreply.github.com>
2019-08-19 11:49:18 +0200

    set up exercise cesar chiffre

---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/cesarChiffre/CesarChiffre.java
@@ -0,0 +1,9 @@
+package at.bfi.coders.bay.exercises.unit2._10.cesarChiffre;
+
+public class CesarChiffre {
+
+	public static String encode(String text, int pwd) {
+		return text;
+	}
+
+}
---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/cesarChiffre/CesarChiffreTester.java
@@ -0,0 +1,34 @@
+package at.bfi.coders.bay.exercises.unit2._10.cesarChiffre;
+
+/**
+Aufgabe: Cäsar Chiffre (Advanced)
+Erstelle ein Programm, das eine Variable mit Text enthält. Dein Programm soll diesen Text mit einem Passwort (hier einer Zahl) durch eine Cäsar-Chiffre verschlüsseln, 
+und dann auf der Konsole ausgeben.
+
+Cäsar Chiffre: Bedeutet dass ich das ABC um eine bestimmte Stelle Verschiebe. Nehmen wir an ich verschiebe es um 2 Positionen dann wird aus A ein C, aus B ein D, aus C ein E usw.
+
+Zusatz:
+
+Der Text soll aus einer Datei eingelesen werden.
+Der chiffrierte Text soll in einer neuen Datei gespeichert werden.
+
+Entschlüsseln
+Erweitere dein Programm so, dass es einen verschlüsselten Text bei gegebenem Passwort entschlüsseln kann. Speichere den entschlüsselten Text in einer separaten Datei.
+
+Schlüssel knacken
+Erweitere dein Programm so, dass es in der Lage ist, einen beliebigen mit einer Cäsar-Chiffre verschlüsselten Text zu entschlüsseln. 
+ * @author groiq
+ *
+ */
+public class CesarChiffreTester {
+
+	public static void main(String[] args) {
+		
+		String test1 = "The quick brown fox jumps over the lazy dog.";
+		int pwd1 = 1;
+		String result1 = CesarChiffre.encode(test1,pwd1);
+		System.out.println(result1);
+
+	}
+
+}
