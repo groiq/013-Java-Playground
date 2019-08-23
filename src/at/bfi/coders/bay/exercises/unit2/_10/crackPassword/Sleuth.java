b479ffd07bc05355d98bf09e4d576c4901c4b2ef
groiq <35578027+groiq@users.noreply.github.com>
2019-08-23 08:49:12 +0200

    reboot crackPassword method

--- src/at/bfi/coders/bay/exercises/unit2/_10/crackPassword/Sleuth.java
+++ src/at/bfi/coders/bay/exercises/unit2/_10/crackPassword/Sleuth.java
@@ -2,6 +2,7 @@ package at.bfi.coders.bay.exercises.unit2._10.crackPassword;
 
 import java.util.Arrays;
 
+
 /**
  * Cracks a password from a professor object.
  * 
@@ -25,61 +26,34 @@ import java.util.Arrays;
  *
  */
 public class Sleuth {
-	
-	private char[] chars;
-	private char[] guess;
-	private char[] password;
-	private Professor professor;
-	private boolean verbose;
 
 	/**
-	 * Returns the professor currently monitored.
-	 * @return the professor currently monitored.
+	 * @param drPepper
+	 * @param b
+	 * @return
 	 */
-	public Professor getProfessor() {
-		return professor;
-	}
-
-	/**
-	 * Assigns a professor to be monitored
-	 * @param professor the professor to be monitored.
-	 */
-	public void setProfessor(Professor professor) {
-		this.professor = professor;
-	}
-
-	/**
-	 * Returns the password the sleuth found.
-	 * @return the password the sleuth found.
-	 */
-	public String getPassword() {
-		return new String(password);
+	public String crackPassword(Professor professor, boolean verbose) {
+		
+		return new String (crackPasswordAsCharArray(professor, verbose));
 	}
 	
-	/**
-	 * Overwrites the stored password.
-	 * @param password the new password.
-	 */
-	public void setPassword(String password) {
-		this.password = password.toCharArray();
+	
+	public char[] crackPasswordAsCharArray(Professor professor, boolean verbose) {
+		
+		int pwdLen = professor.getPasswordLength();
+		char[] chars = professor.getPwdChars();
+		char[] guess = new char[pwdLen];
+		int position = 0;
+		
+		
+		return guess;
+		
 	}
-
-	/**
-	 * Returns the password the sleuth found as a char[].
-	 * @return the password the sleuth found as a char[]
-	 */
-	public char[] getPasswordAsCharArray() {
-		return password;
-	}
-
-	/**
-	 * Overwrites the password.
-	 * @param password the new password.
-	 */
-	public void setPasswordAsCharArray(char[] password) {
-		this.password = password;
-	}
-
+	
+	
+	
+	
+// legacy method comments
 	
 	/*
 	 * Runs through all possible values of the char at the current position.
@@ -88,42 +62,11 @@ public class Sleuth {
 	 * If there are more characters in the password, run this method on the next position.
 	 */
 	/* next milestone: stop running through possibilities once the password is found */
-	private void tryForPosition(int pos) {
-		for (char c : chars) {
-			guess[pos] = c;
-			if (pos == guess.length - 1) {
-				if (this.verbose) {
-					System.out.println("current guess: " + new String(guess));
-				}
-//				System.out.println(Arrays.toString(guess));
-				if (professor.enterPassword(guess)) {
-					this.password = Arrays.copyOf(guess, guess.length);
-					if (verbose) {
-						System.out.println("Password found!");
-						}
-					return;
-				}
-			} else {
-				tryForPosition(pos+1);
-
-			}
-		}
-	}
 	
 	/*
 	 * Assign the local fields, then call tryForPosition().
 	 * When the password is found, fetch and return the solution.
 	 */
-	public String crackPassword(Professor professor,boolean verbose) {
-		this.verbose = verbose;
-		this.professor = professor;
-		int pwdLen = professor.getPasswordLength();
-		chars = professor.getPwdChars();
-		guess = new char[pwdLen];
-		tryForPosition(0);
-		return new String(this.password);
-	}
 	
-		
 
 }
