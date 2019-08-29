c4f8feeed44c96c9d62ae3dc42b5ac8511dd80f1
groiq <35578027+groiq@users.noreply.github.com>
2019-08-29 17:32:54 +0200

    set up photograpy exercise

---
+++ src/at/bfi/coders/bay/exercises/unit5/photography/PhotographyApp.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package at.bfi.coders.bay.exercises.unit5.photography;
+
+/**
+ * <h1>Photograpy Simulator exercise</h1>
+ * 
+ * <h2>Assignment:</h2>
+ * 
+ * Erstelle eine Klasse Camera mit folgenden Attributen: megaPixel, displaySize
+ * , isStable, brand, focalDistanceMin, focalDistanceMax<br>
+ * Diese Klasse hat eine Methode "takePhoto" welche einfach "Klick" ausgiebt<br>
+ * Lege für jedes einzelne Attribut deiner Klasse get und set Methoden an. Eine
+ * Get Methode liefert immer den Wert des Attributes, und die setMethode setzt
+ * den Wert<br>
+ * Für setfocalDistanceMin muss überprüft werden, dass der Wert kleiner ist als
+ * in Max, sowie in setFocalDistanceMax muss überprüft werden ob er größer ist
+ * als das Minimum<br>
+ * Für das setzen der Werte möchte ich nur EINE Methode setBrennweite welche
+ * beide Werte übergeben bekommt<br>
+ * Hauptprogramm: Erzeuge mindestens 3 verschiedene Fotoapparate<br>
+ * Zusatz 1:<br>
+ * <br>
+ * Wähle je nach Wochentag eine andere Kamera aus<br>
+ * Am Wochenende möchtest du aufs Land fahren und zusätzliche Materialien
+ * mitnehmen. Baue dir eine Erinnerung falls es Sa oder Sonntag ist, deine
+ * Materialien einzupacken (Verwendung von Date-Format)<br>
+ * Passe deine Methode takePhoto so an, dass du jedesmal ein neues Foto
+ * erstellst welches einen Namen hat (fortlaufende Nummer), die pixelCount, die
+ * size und ob es schwarz-weiß oder farbe fotografiert wurde<br>
+ * <br>
+ * <br>
+ * Zusatz 2:<br>
+ * <br>
+ * Erstelle eine neue Klasse Objective mit den Eigenschaften focalDistance
+ * Maximum und Minimum<br>
+ * Lege eine neue Objektvariable für Objective in der Klasse Kamera an<br>
+ * Passe dein Programm so an dass es wieder funktioniert.<br>
+ * 
+ * 
+ * @author groiq
+ * @version
+ * @since 27.08.2019
+ * 
+ */
+public class PhotographyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
