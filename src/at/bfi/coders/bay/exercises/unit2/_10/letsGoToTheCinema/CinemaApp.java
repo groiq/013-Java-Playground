1a9b783db04d2daf4cfc165e1fe405786465435e
groiq <35578027+groiq@users.noreply.github.com>
2019-09-18 09:39:01 +0200

    set up a new Cinema app

---
+++ src/at/bfi/coders/bay/exercises/unit2/_10/letsGoToTheCinema/CinemaApp.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package at.bfi.coders.bay.exercises.unit2._10.letsGoToTheCinema;
+
+/**
+ * <h1>Cinema simulator</h1>
+ * 
+ * Assignment:<br>
+ * 
+ * Aufgabe: Gehen wir ins Kino<br>
+ * <br>
+ * Im Kino seht ihr auf dem Monitor das Kinoprogramm. Dieses besteht aus
+ * verschiedensten Filmen, Uhrzeiten, einen Kinosaal, ausgebucht (j/n) und eine
+ * Info ob der Film in 3D ist oder nicht. Erstelle eine kleine Anzeigentafel in
+ * der Konsole in der du ein paar aktuelle Filme du du zb bei Cineplexx findest,
+ * anzeigst<br>
+ * <br>
+ * Gehe zum Ticketschalter und hole dir dein Ticket. Das Ticket musst du
+ * abspeichern denn ansonsten darfst du nicht in den Kinosaal.<br>
+ * <br>
+ * Schaue auf die Uhr und finde heraus wie spät es ist.<br>
+ * <br>
+ * Hast du noch mehr als 10 Minuten Zeit, dann kannst du noch aufs WC gehen UND
+ * dir etwas zu essen holen<br>
+ * Hast du weniger als 10 Minuten Zeit reicht es leider nur mehr für den Gang
+ * zur Toilette<br>
+ * Wenn du in Punkt 3 herausgefunden hast, dass du noch mehr als 10 Minuten Zeit
+ * hast, dann kannst du dir noch etwas zu essen holen. Du kannst dir alles
+ * kaufen was du gerne möchtest, allerdings musst du überprüfen ob du genug Geld
+ * mit hast. Der Preis der Produkte darf also nicht höher sein, als die Menge an
+ * Geld die du in der Geldtasche hast. ACHTUNG: Das Kino hat heute eine
+ * besondere Aktion, jeder Kunde darf eine Zahl ziehen. Wenn diese Zahl eine
+ * Primzahl (2,3,5,7...usw) ist ODER aber eine Runde Zahl (10,20,30...), dann
+ * brauchst du den Einkauf nicht bezahlen. Generiere mittels Random eine
+ * Zufallszahl und versuch dein Glück<br>
+ * 
+ * 
+ * @author Hannes Alkin
+ * @version
+ * @since 16.09.2019
+ * 
+ */
+public class CinemaApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("start up program...");
+		System.out.println("look at screen");
+		System.out.println("try buying a sold out ticket");
+		System.out.println("buy a ticket");
+		System.out.println("check watch");
+		System.out.println("first case: too little time left");
+		
+
+	}
+
+}
