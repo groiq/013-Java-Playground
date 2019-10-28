cf370c25f463d6c2b659b9b624699f3d72c4f954
groiq <35578027+groiq@users.noreply.github.com>
2019-10-28 09:05:23 +0100

    airport exercise: set up first exercise

---
+++ src/at/bfi/coders/bay/exercises/unit6/flughafen/_1/Employee.java
@@ -0,0 +1,13 @@
+package at.bfi.coders.bay.exercises.unit6.flughafen._1;
+
+/**
+ * 
+ * 
+ * @author  Hannes Alkin
+ * @version 
+ * @since   28.10.2019
+
+ */
+public class Employee {
+
+}
---
+++ src/at/bfi/coders/bay/exercises/unit6/flughafen/_1/Flughafen1App.java
@@ -0,0 +1,71 @@
+package at.bfi.coders.bay.exercises.unit6.flughafen._1;
+
+/**
+ * <h1>Flughafen</h1>
+ * 
+ * assignment:
+ * 
+ * Übung 1: Generierung der Klassen employees und der Klasse flights
+ * 
+ * Die Klasse employee hat folgende Eigenschaften:
+ * 
+ * personnel_number:int (Primärschlüssel)
+ * 
+ * first_name: string
+ * 
+ * last_name: string
+ * 
+ * street: string
+ * 
+ * appartment_number: string
+ * 
+ * zip: string
+ * 
+ * phonenumber: string
+ * 
+ * email: string
+ * 
+ * flights hat folgende Eigenschaften:
+ * 
+ * flight_number
+ * 
+ * departure: Time
+ * 
+ * flight_duration: int
+ * 
+ * departure_gate: string
+ * 
+ * arrival_gate: string
+ * 
+ * pilot: Employee
+ * 
+ * co_pilot: Employee
+ * 
+ * Step 1: Erzeugen der Klassen in Java
+ * 
+ * Step 2: Erzeugen von mehreren Employees und Flügen mit tatsächlichen
+ * Referenzen auf die vorher erzeugten Employees.
+ * 
+ * Step 3: Flugsuche: Ihr könnt eine Flugnummer in der Konsole eingeben und
+ * findet den entsprechenden Flug dazu
+ * 
+ * Step 4: Flüge der Piloten: Ihr könnt eine Personalnummer in der Konsole
+ * eingeben und findet alle Flüge die dieser Pilot als Pilot oder als Co-Pilot
+ * durchführt
+ * 
+ * @author Hannes Alkin
+ * @version
+ * @since 28.10.2019
+ * 
+ */
+public class Flughafen1App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
